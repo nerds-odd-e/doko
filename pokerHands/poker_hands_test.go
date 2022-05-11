775523434f7476216ed66df8ce10df6e380dfc90
tedmund + ivan <foo@bar.com>
2022-05-11 15:46:04 +0800

    refactor

--- pokerHands/poker_hands_test.go
+++ pokerHands/poker_hands_test.go
@@ -7,18 +7,52 @@ import (
 )
 
 func Test_poker_empty(t *testing.T) {
-	hands := ""
-	assert.Equal(t, 0, p1_win_counts(hands))
+	game := ""
+	assert.Equal(t, 0, p1_win_counts(game))
 }
 
 func Test_poker_p1_wins_high_card(t *testing.T) {
-	hands := "9C 2S 2C 2H 4S 2D 8S 3D 3S 3C"
-	assert.Equal(t, 1, p1_win_counts(hands))
+	p1 := "9C 2D 2C 3D 3C"
+	p2 := "8S 2K 2S 3K 3S"
+	game := p1 + " " + p2
+	assert.Equal(t, 1, p1_win_counts(game))
 }
 
-func p1_win_counts(hands string) int {
-	if hands == "" {
+func Test_poker_p2_wins_high_card(t *testing.T) {
+	p1 := "8C 2D 2C 3D 3C"
+	p2 := "9S 2K 2S 3K 3S"
+	game := p1 + " " + p2
+	assert.Equal(t, 0, p1_win_counts(game))
+}
+
+// func Test_poker_p1_wins_high_card_2_games(t *testing.T) {
+// 	p1 := "9C 2D 2C 3D 3C"
+// 	p2 := "8S 2K 2S 3K 3S"
+// 	p1_2:= "9C 2D 2C 3D 3C"
+// 	p2_2 := "8S 2K 2S 3K 3S"
+// 	hands := p1 + " " + p2 + "\n"+ p1 + " " + p2
+// 	assert.Equal(t, 2, p1_win_counts(hands))
+// }
+
+func p1_win_counts(games string) int {
+
+	if games == "" {
 		return 0
 	}
-	return 1
+
+	// game_list := strings.Split(games, "\n")
+
+	// for game :=
+
+	if get_high_card(games, "p1") > get_high_card(games, "p2") {
+		return 1
+	}
+	return 0
+}
+
+func get_high_card(games string, player string) byte {
+	if player == "p1" {
+		return games[0]
+	}
+	return games[15]
 }
