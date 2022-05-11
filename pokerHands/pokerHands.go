d26bd4657658b0bc1327b90385e9a6ea3a2dc223
Janice Tan <janice_yp_tan@tech.gov.sg>
2022-05-11 14:57:12 +0800

    Wrote Test on 0 and 1 Inputs

--- pokerHands/pokerHands.go
+++ pokerHands/pokerHands.go
@@ -1,1 +1,16 @@
 package pokerhands
+
+import "strings"
+
+func pokerHands(hands []string) int {
+	if len(hands) == 1 {
+
+		cards := strings.Fields(hands[0])
+
+		if cards[4] > cards[9] {
+			return 1
+		}
+	}
+
+	return 0
+}
--- pokerHands/pokerHands_test.go
+++ pokerHands/pokerHands_test.go
@@ -1,7 +1,40 @@
 package pokerhands
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestEmptyInput(t *testing.T) {
-
+	input := []string{}
+	output := pokerHands(input)
+	assert.Equal(t, 0, output)
 }
+
+func TestSingleInputP1Wins(t *testing.T) {
+	input := []string{"3S 4H 5S 7S 9S 2D 3D 5D 7D 8S"}
+	output := pokerHands(input)
+	assert.Equal(t, 1, output)
+}
+
+func TestSingleInputP2Wins(t *testing.T) {
+	input := []string{"2D 3D 5D 7D 8S 3S 4H 5S 7S 9S"}
+	output := pokerHands(input)
+	assert.Equal(t, 0, output)
+}
+
+// func readFile(fileName string) string {
+// 	file, err := os.Open(fileName)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer func() {
+// 		if err = file.Close(); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}()
+
+// 	b, err := ioutil.ReadAll(file)
+// 	return string(b)
+// }
