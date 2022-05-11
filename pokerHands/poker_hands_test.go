a195ae8f5c5e88f9475e30046f8baeda928961c3
tedmund + ivan <foo@bar.com>
2022-05-11 14:52:24 +0800

    high card one line

--- pokerHands/poker_hands_test.go
+++ pokerHands/poker_hands_test.go
@@ -8,9 +8,17 @@ import (
 
 func Test_poker_empty(t *testing.T) {
 	hands := ""
-	assert.Equal(t, 0, poker(hands))
+	assert.Equal(t, 0, p1_win_counts(hands))
 }
 
-func poker(hands string) int {
-	return 0
+func Test_poker_p1_wins_high_card(t *testing.T) {
+	hands := "9C 2S 2C 2H 4S 2D 8S 3D 3S 3C"
+	assert.Equal(t, 1, p1_win_counts(hands))
+}
+
+func p1_win_counts(hands string) int {
+	if hands == "" {
+		return 0
+	}
+	return 1
 }
