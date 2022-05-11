7f7247917670d071c12b12c52febc1af550b1fae
Janice Tan <janice_yp_tan@tech.gov.sg>
2022-05-11 14:25:09 +0800

    Fizz Buzz Exercise

---
+++ fizzbuzz/fizzbuzz.go
@@ -0,0 +1,17 @@
+package fizzbuzz
+
+import "fmt"
+
+func FizzBuzz(n int) string {
+	if n%3 == 0 && n%5 == 0 {
+		return "FizzBuzz"
+	}
+	if n%3 == 0 {
+		return "Fizz"
+	}
+	if n%5 == 0 {
+		return "Buzz"
+	}
+	return fmt.Sprint(n)
+
+}
