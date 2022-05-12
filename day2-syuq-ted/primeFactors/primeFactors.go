69c92fa44bcf42ae529f240bb6d42b60eba1d1b2
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-12 14:19:16 +0800

    tcring

--- day2-syuq-ted/primeFactors/primeFactors.go
+++ day2-syuq-ted/primeFactors/primeFactors.go
@@ -1,11 +1,8 @@
 package primeFactors
 
 func primeFactors(number int) []int {
-	if number == 2 {
-		return []int{2}
+	if number == 1 {
+		return []int{}
 	}
-	if number == 3 {
-		return []int{3}
-	}
-	return []int{}
+	return []int{number}
 }
