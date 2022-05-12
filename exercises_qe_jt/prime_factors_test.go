cf3fc516512e178fd2295c3f3811e7aa09a7a1eb
Qing En <qingen@hotmail.sg>
2022-05-12 11:34:14 +0800

    tcring

--- exercises_qe_jt/prime_factors_test.go
+++ exercises_qe_jt/prime_factors_test.go
@@ -7,14 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func PrimeFactors(n int) string {
-	if n % 2 == 0 {
-		return "2"
-	}
-	return fmt.Sprintf("%v", n)
-}
-
-func PrimeFactors1(n int) []int {
+func PrimeFactors(n int) []int {
 	if n % 2 == 0 {
 		return []int{2}
 	}
@@ -22,22 +15,22 @@ func PrimeFactors1(n int) []int {
 }
 
 func TestPrimeFactors2(t * testing.T) {
-	fmt.Println(PrimeFactors1(2))
-	assert.Equal(t, []int{2}, PrimeFactors1(2))
+	fmt.Println(PrimeFactors(2))
+	assert.Equal(t, []int{2}, PrimeFactors(2))
 }
 
 func TestPrimeFactors3(t * testing.T) {
-	assert.Equal(t, []int{3}, PrimeFactors1(3))
+	assert.Equal(t, []int{3}, PrimeFactors(3))
 }
 
 func TestPrimeFactors4(t * testing.T) {
-	assert.Equal(t, []int{2}, PrimeFactors1(4))
+	assert.Equal(t, []int{2}, PrimeFactors(4))
 }
 
 func TestPrimeFactors5(t * testing.T) {
-	assert.Equal(t, []int{5}, PrimeFactors1(5))
+	assert.Equal(t, []int{5}, PrimeFactors(5))
 }
 
 func TestPrimeFactors6(t * testing.T) {
-	assert.Equal(t, []int{2}, PrimeFactors1(6))
+	assert.Equal(t, []int{2}, PrimeFactors(6))
 }
