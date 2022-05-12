928217c95bfbf9bcb3960227454a22a50dd9f4e4
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-12 11:58:04 +0800

    tcring

--- day2-syuq-ted/primeFactors/primeFactors.go
+++ day2-syuq-ted/primeFactors/primeFactors.go
@@ -1,19 +1,5 @@
 package primeFactors
 
 func primeFactors(number int) []int {
-	if number == 1 {
-		return []int{}
-	}
-
-	factors := []int{}
-	if number%2 == 0 {
-		factors = append(factors, 2)
-	}
-	if number%3 == 0 {
-		factors = append(factors, 3)
-	}
-	if number%5 == 0 {
-		factors = append(factors, 5)
-	}
-	return factors
+	return []int{}
 }
--- day2-syuq-ted/primeFactors/primeFactors_test.go
+++ day2-syuq-ted/primeFactors/primeFactors_test.go
@@ -8,18 +8,4 @@ import (
 
 func TestOnePrimeFactors(t *testing.T) {
 	assert.Equal(t, []int{}, primeFactors(1))
-
-	assert.Equal(t, []int{2}, primeFactors(2))
-	assert.Equal(t, []int{3}, primeFactors(3))
-	assert.Equal(t, []int{5}, primeFactors(5))
-
-	// squares
-	assert.Equal(t, []int{2}, primeFactors(4))
-	assert.Equal(t, []int{3}, primeFactors(9))
-	assert.Equal(t, []int{5}, primeFactors(25))
-}
-
-func TestTwoPrimeFactors(t *testing.T) {
-	assert.Equal(t, []int{2, 3}, primeFactors(6))
-	assert.Equal(t, []int{2, 5}, primeFactors(10))
 }
