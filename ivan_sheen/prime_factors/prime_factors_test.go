9d06376dffbe1d2d264b35650594a329a40feab1
tedmund + ivan <foo@bar.com>
2022-05-12 11:22:35 +0800

    tcring

--- ivan_sheen/prime_factors/prime_factors_test.go
+++ ivan_sheen/prime_factors/prime_factors_test.go
@@ -17,3 +17,7 @@ func TestPrimeFactors2(t *testing.T) {
 func TestPrimeFactors3(t *testing.T) {
 	assert.Equal(t, []int{3}, primeFactors(3))
 }
+
+func TestPrimeFactors4(t *testing.T) {
+	assert.Equal(t, []int{3}, primeFactors(3))
+}
