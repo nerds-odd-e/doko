ed5c1a7393f8a2426c4f3e7eaa37104d1d4a01ee
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-12 11:19:17 +0800

    tcring

--- day2-syuq-ted/primeFactors/primeFactors.go
+++ day2-syuq-ted/primeFactors/primeFactors.go
@@ -1,5 +1,8 @@
 package primeFactors
 
 func primeFactors(number int) []int {
+	if number == 3 {
+		return []int{3}
+	}
 	return []int{2}
 }
--- day2-syuq-ted/primeFactors/primeFactors_test.go
+++ day2-syuq-ted/primeFactors/primeFactors_test.go
@@ -8,5 +8,5 @@ import (
 
 func TestOnePrimeFactors(t *testing.T) {
 	assert.Equal(t, []int{2}, primeFactors(2))
-	assert.Equal(t, []int{2}, primeFactors(3))
+	assert.Equal(t, []int{3}, primeFactors(3))
 }
