1a1561c6ba79203fc4567d9571181c9c511a455b
Qing En <qingen@hotmail.sg>
2022-05-12 10:29:37 +0800

    tcring

--- exercises_qe_jt/fizz_buzz_test.go
+++ exercises_qe_jt/fizz_buzz_test.go
@@ -7,9 +7,16 @@ import (
 )
 
 func FizzBuzz(number int)string{
-	return "1"
+	if number == 1 {
+		return "1"
+	}
+	return "2"
 }
 
 func Test1Return1(t *testing.T){
 	assert.Equal(t,"1",FizzBuzz(1))
+}
+
+func Test2Return2(t *testing.T){
+	assert.Equal(t,"2",FizzBuzz(2))
 }
