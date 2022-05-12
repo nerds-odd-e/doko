1a4d67401ae2ef7d2bcdd12000bdddcc583c0a57
Qing En <qingen@hotmail.sg>
2022-05-12 10:27:48 +0800

    tcring

---
+++ exercises_qe_jt/fizz_buzz_test.go
@@ -0,0 +1,15 @@
+package exercises_qe_jt
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func FizzBuzz(number int)string{
+	return ""
+}
+
+func Test1Return1(t *testing.T){
+	assert.Equal(t,"",FizzBuzz(1))
+}
