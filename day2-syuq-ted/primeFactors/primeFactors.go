98d3f89ee4c20170967a51543628962853096151
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-12 11:20:50 +0800

    tcring

--- day2-syuq-ted/primeFactors/primeFactors.go
+++ day2-syuq-ted/primeFactors/primeFactors.go
@@ -4,5 +4,8 @@ func primeFactors(number int) []int {
 	if number == 3 {
 		return []int{3}
 	}
+	if number == 5 {
+		return []int{5}
+	}
 	return []int{2}
 }
--- day2-syuq-ted/primeFactors/primeFactors_test.go
+++ day2-syuq-ted/primeFactors/primeFactors_test.go
@@ -10,5 +10,5 @@ func TestOnePrimeFactors(t *testing.T) {
 	assert.Equal(t, []int{2}, primeFactors(2))
 	assert.Equal(t, []int{3}, primeFactors(3))
 	assert.Equal(t, []int{2}, primeFactors(4))
-	assert.Equal(t, []int{2}, primeFactors(5))
+	assert.Equal(t, []int{5}, primeFactors(5))
 }
