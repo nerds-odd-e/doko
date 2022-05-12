194323b138c840d570b5927742fda58df26ff561
Qing En <qingen@hotmail.sg>
2022-05-12 10:52:26 +0800

    tcring

--- exercises_qe_jt/prime_factors_test.go
+++ exercises_qe_jt/prime_factors_test.go
@@ -7,6 +7,9 @@ import (
 )
 
 func PrimeFactors(n int) string {
+	if n == 3 {
+		return "3"
+	}
 	return "2"
 }
 
@@ -15,5 +18,5 @@ func TestPrimeFactors2(t * testing.T) {
 }
 
 func TestPrimeFactors3(t * testing.T) {
-	assert.Equal(t, "2", PrimeFactors(3))
+	assert.Equal(t, "3", PrimeFactors(3))
 }
