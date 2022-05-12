86803888b8a3452c461ab8ef8a7ab4a14c3a3007
tedmund + ivan <foo@bar.com>
2022-05-12 14:28:46 +0800

    tcring

--- sheean_ys_pokerhands/pokerHands_test.go
+++ sheean_ys_pokerhands/pokerHands_test.go
@@ -5,17 +5,23 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-
+func HighCardHandSorted() string{
+ return "2C 3D 4D 5C 7C"
+}
+func LowCardHandSorted() string{
+ return "2C 3H 4D 5D 6S"
+}
 func TestNoGames(t *testing.T) {
 	assert.Equal(t, 0, pokerHands([]string{}))
 }
 
-func TestHighCard(t *testing.T) {
-	//
-	assert.Equal(t, 0, pokerHands([]string{"2C 3D 4D 5C 6C 2C 3H 4D 5D 7S"}))
-	assert.Equal(t, 1, pokerHands([]string{"2C 3D 4D 5C 7C 2C 3H 4D 5D 6S"}))
+func TestPlayer1WinsWithHighCard(t *testing.T) {
+	assert.Equal(t, 1, pokerHands([]string{HighCardHandSorted() + " " + LowCardHandSorted()}))
 }
-func Test_One_Game_Unsorted(t *testing.T) {
+func TestPlayer2WinsWithHighCard(t *testing.T) {
+	assert.Equal(t, 0, pokerHands([]string{"2C 3D 4D 5C 6C 2C 3H 4D 5D 7S"}))
+}
+func TestOneGameUnsorted(t *testing.T) {
 	assert.Equal(t, 0, pokerHands([]string{"7C 3D 4D 5C 2C 2C 6H 4D 5D 9S"}))
 	assert.Equal(t, 1, pokerHands([]string{"9C 3D 4D 5C 2C 2C 6H 4D 5D 7S"}))
 }
