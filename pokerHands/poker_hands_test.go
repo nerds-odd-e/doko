471e9be0f5be87a052aa8432f4b1c457066ca0a3
tedmund + ivan <foo@bar.com>
2022-05-12 09:02:55 +0800

    last change

--- pokerHands/poker_hands_test.go
+++ pokerHands/poker_hands_test.go
@@ -1,6 +1,7 @@
 package pokerHands
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -25,29 +26,34 @@ func Test_poker_p2_wins_high_card(t *testing.T) {
 	assert.Equal(t, 0, p1_win_counts(game))
 }
 
-// func Test_poker_p1_wins_high_card_2_games(t *testing.T) {
-// 	p1 := "9C 2D 2C 3D 3C"
-// 	p2 := "8S 2K 2S 3K 3S"
-// 	p1_2:= "9C 2D 2C 3D 3C"
-// 	p2_2 := "8S 2K 2S 3K 3S"
-// 	hands := p1 + " " + p2 + "\n"+ p1 + " " + p2
-// 	assert.Equal(t, 2, p1_win_counts(hands))
-// }
+func Test_poker_p1_wins_high_card_2_games(t *testing.T) {
+	p1 := "9C 2D 2C 3D 3C"
+	p2 := "8S 2K 2S 3K 3S"
+	games := p1 + " " + p2 + "\n" + p1 + " " + p2
+	assert.Equal(t, 2, p1_win_counts(games))
+}
 
 func p1_win_counts(games string) int {
 
+	wins := 0
+	for _, game := range toGames(games) {
+		if check_if_p1_win_high_card(game) {
+			wins += 1
+		}
+	}
+	return wins
+}
+
+func toGames(games string) []string {
 	if games == "" {
-		return 0
+		return []string{}
 	}
 
-	// game_list := strings.Split(games, "\n")
+	return strings.Split(games, "\n")
+}
 
-	// for game :=
-
-	if get_high_card(games, "p1") > get_high_card(games, "p2") {
-		return 1
-	}
-	return 0
+func check_if_p1_win_high_card(game string) bool {
+	return get_high_card(game, "p1") > get_high_card(game, "p2")
 }
 
 func get_high_card(games string, player string) byte {
