9a60233b2b65c8e8a07772d9cece823aaf19ca05
tedmund + ivan <foo@bar.com>
2022-05-12 14:35:44 +0800

    tcring

--- sheean_ys_pokerhands/pokerHands_test.go
+++ sheean_ys_pokerhands/pokerHands_test.go
@@ -5,21 +5,22 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-func HighCardHandSorted() string{
- return "2C 3D 4D 5C 7C"
+
+func HighCardHandSorted() string {
+	return "2C 3D 4D 5C 7C"
 }
-func LowCardHandSorted() string{
- return "2C 3H 4D 5D 6S"
+func LowCardHandSorted() string {
+	return "2C 3H 4D 5D 6S"
 }
 func TestNoGames(t *testing.T) {
 	assert.Equal(t, 0, pokerHands([]string{}))
 }
 
-func HighCardHandUnSorted() string{
- return "2C 6H 4D 5D 9S"
+func HighCardHandUnSorted() string {
+	return "2C 6H 4D 5D 9S"
 }
-func LowCardHandUnSorted() string{
- return "7C 3D 4D 5C 2C"
+func LowCardHandUnSorted() string {
+	return "7C 3D 4D 5C 2C"
 }
 func TestPlayer1WinsWithHighCard(t *testing.T) {
 	assert.Equal(t, 1, pokerHands([]string{HighCardHandSorted() + " " + LowCardHandSorted()}))
@@ -32,8 +33,16 @@ func TestOneGameUnsorted(t *testing.T) {
 	assert.Equal(t, 1, pokerHands([]string{HighCardHandUnSorted() + " " + LowCardHandUnSorted()}))
 }
 
+func HighestCardSameRankSecondHigher() string {
+	return "2C 6H 4D 5D 9S"
+}
+
+func HighestCardSameRankSecondLower() string {
+	return "9C 3D 4D 5C 2C"
+}
+
 func TestOneGameSameRank(t *testing.T) {
-	assert.Equal(t, 0, pokerHands([]string{"9C 3D 4D 5C 2C 2C 6H 4D 5D 9S"}))
+	assert.Equal(t, 0, pokerHands([]string{HighestCardSameRankSecondLower() + " " + HighestCardSameRankSecondHigher()}))
 	assert.Equal(t, 1, pokerHands([]string{"9S 3D 4D 5C 2C 2C 6H 4D 5D 9C"}))
 }
 func TestOneGameWithCourtCards(t *testing.T) {
