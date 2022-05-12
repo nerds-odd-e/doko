4011b3ee5ee6be2b4d448899e44828a252e3a863
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-12 11:23:14 +0800

    tcring

--- day2-syuq-ted/primeFactors/primeFactors_test.go
+++ day2-syuq-ted/primeFactors/primeFactors_test.go
@@ -9,6 +9,10 @@ import (
 func TestOnePrimeFactors(t *testing.T) {
 	assert.Equal(t, []int{2}, primeFactors(2))
 	assert.Equal(t, []int{3}, primeFactors(3))
-	assert.Equal(t, []int{2}, primeFactors(4))
 	assert.Equal(t, []int{5}, primeFactors(5))
+
+	// squares
+	assert.Equal(t, []int{2}, primeFactors(4))
+	assert.Equal(t, []int{2}, primeFactors(9))
+	assert.Equal(t, []int{2}, primeFactors(25))
 }
