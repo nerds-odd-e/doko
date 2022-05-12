c0ea7893d4bb3916fc93079de198e5515cdcb32c
Qing En <qingen@hotmail.sg>
2022-05-12 11:19:41 +0800

    tcring

--- exercises_qe_jt/prime_factors_test.go
+++ exercises_qe_jt/prime_factors_test.go
@@ -19,10 +19,14 @@ func TestPrimeFactors3(t * testing.T) {
 	assert.Equal(t, "3", PrimeFactors(3))
 }
 
+func TestPrimeFactors4(t * testing.T) {
+	assert.Equal(t, "", PrimeFactors(4))
+}
+
 func TestPrimeFactors5(t * testing.T) {
 	assert.Equal(t, "5", PrimeFactors(5))
 }
 
-func TestPrimeFactors6(t * testing.T) {
+func xTestPrimeFactors6(t * testing.T) {
 	assert.Equal(t, "6", PrimeFactors(6))
 }
