15630421655344b9fdd004099bd0d8d1d215936e
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-12 14:20:33 +0800

    tcring

--- day2-syuq-ted/primeFactors/primeFactors_test.go
+++ day2-syuq-ted/primeFactors/primeFactors_test.go
@@ -12,4 +12,5 @@ func TestReturnEmpty(t *testing.T) {
 func TestReturnOne(t *testing.T) {
 	assert.Equal(t, []int{2}, primeFactors(2))
 	assert.Equal(t, []int{3}, primeFactors(3))
+	assert.Equal(t, []int{4}, primeFactors(4))
 }
