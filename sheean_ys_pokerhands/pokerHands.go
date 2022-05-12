311c8b8555c589fa25cf8229c23cc315de558169
tedmund + ivan <foo@bar.com>
2022-05-12 15:28:37 +0800

    tcring

--- sheean_ys_pokerhands/pokerHands.go
+++ sheean_ys_pokerhands/pokerHands.go
@@ -5,6 +5,25 @@ import (
 	"strings"
 )
 
+// Game
+// Deck (2 hands)
+// Hand
+// Player
+// Card
+// Rank (e.g. A,2,3,...T,J,Q,K)
+// Suite(e.g. D,H,C,S)
+
+type Card struct {
+	Rank  int
+	Suite string
+}
+
+// Constructor
+func NewCard(card string) *Card {
+	c := &Card{getRank(card[:1]), card[1:]}
+	return c
+}
+
 var RankMap = map[string]int{
 	"T": 10,
 	"J": 11,
@@ -17,6 +36,8 @@ func pokerHands(hands []string) int {
 	player1Points := 0
 	for _, hand := range hands {
 		cards := strings.Split(hand, " ")
+
+		// card := NewCard(cards[0])
 
 		p1HighestRank, p1HighestSuite := getHighestCard(cards[:5])
 		p2HighestRank, p2HighestSuite := getHighestCard(cards[5:])
@@ -31,16 +52,20 @@ func pokerHands(hands []string) int {
 	return player1Points
 }
 
+func getRank(rank string) int {
+	rankInt, err := strconv.Atoi(rank)
+
+	if err != nil {
+		rankInt = RankMap[rank]
+	}
+	return rankInt
+}
+
 func getHighestCard(hand []string) (int, string) {
 	currentHighestRank := 0
 	highestSuite := ""
 	for _, card := range hand {
-		cardRankStr := string(card[0])
-		cardRank, err := strconv.Atoi(cardRankStr)
-
-		if err != nil {
-			cardRank = RankMap[cardRankStr]
-		}
+		cardRank := getRank(string(card[0]))
 		if cardRank > currentHighestRank {
 			currentHighestRank = cardRank
 			highestSuite = string(card[1])
