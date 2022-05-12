32ff01c9cc0e1b7a26b133d41e20e30c6ab0abf2
tedmund + ivan <foo@bar.com>
2022-05-12 14:23:35 +0800

    tcring

---
+++ sheean_ys_pokerhands/pokerHands.go
@@ -0,0 +1,50 @@
+package sheenan_ys_pokerhands
+
+import (
+	"strconv"
+	"strings"
+)
+
+var RankMap = map[string]int{
+	"T": 10,
+	"J": 11,
+	"Q": 12,
+	"K": 13,
+	"A": 14,
+}
+
+func pokerHands(hands []string) int {
+	player1Points := 0
+	for _, hand := range hands {
+		cards := strings.Split(hand, " ")
+
+		p1HighestRank, p1HighestSuite := getHighestCard(cards[:5])
+		p2HighestRank, p2HighestSuite := getHighestCard(cards[5:])
+
+		if p1HighestRank > p2HighestRank {
+			player1Points += 1
+		}
+		if p1HighestRank == p2HighestRank && p1HighestSuite > p2HighestSuite {
+			player1Points += 1
+		}
+	}
+	return player1Points
+}
+
+func getHighestCard(hand []string) (int, string) {
+	currentHighestRank := 0
+	highestSuite := ""
+	for _, card := range hand {
+		cardRankStr := string(card[0])
+		cardRank, err := strconv.Atoi(cardRankStr)
+
+		if err != nil {
+			cardRank = RankMap[cardRankStr]
+		}
+		if cardRank > currentHighestRank {
+			currentHighestRank = cardRank
+			highestSuite = string(card[1])
+		}
+	}
+	return currentHighestRank, highestSuite
+}
---
+++ sheean_ys_pokerhands/pokerHands_test.go
@@ -0,0 +1,41 @@
+package sheenan_ys_pokerhands
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNoGames(t *testing.T) {
+	assert.Equal(t, 0, pokerHands([]string{}))
+}
+
+func TestHighCard(t *testing.T) {
+	//
+	assert.Equal(t, 0, pokerHands([]string{"2C 3D 4D 5C 6C 2C 3H 4D 5D 7S"}))
+	assert.Equal(t, 1, pokerHands([]string{"2C 3D 4D 5C 7C 2C 3H 4D 5D 6S"}))
+}
+func Test_One_Game_Unsorted(t *testing.T) {
+	assert.Equal(t, 0, pokerHands([]string{"7C 3D 4D 5C 2C 2C 6H 4D 5D 9S"}))
+	assert.Equal(t, 1, pokerHands([]string{"9C 3D 4D 5C 2C 2C 6H 4D 5D 7S"}))
+}
+
+func TestOneGameSameRank(t *testing.T) {
+	assert.Equal(t, 0, pokerHands([]string{"9C 3D 4D 5C 2C 2C 6H 4D 5D 9S"}))
+	assert.Equal(t, 1, pokerHands([]string{"9S 3D 4D 5C 2C 2C 6H 4D 5D 9C"}))
+}
+func TestOneGameWithCourtCards(t *testing.T) {
+	assert.Equal(t, 0, pokerHands([]string{"9C JD 4D 5C 2C KC 6H 4D 5D 8S"}))
+	assert.Equal(t, 1, pokerHands([]string{"3S AD 4D 5C 2C 2C 6H QD 5D 9C"}))
+}
+
+func TestOneGamme(t *testing.T) {
+	assert.Equal(t, 0, pokerHands([]string{"2C 3D 4D 5C 6C 2C 3H 4D 5D 7S"}))
+	assert.Equal(t, 1, pokerHands([]string{"2C 3D 4D 5C 7C 2C 3H 4D 5D 6S"}))
+}
+
+func TestTwoGames(t *testing.T) {
+	assert.Equal(t, 0, pokerHands([]string{"9C JD 4D 5C 2C KC 6H 4D 5D 8S", "9C JD 4D 5C 2C KC 6H 4D 5D 8S"}))
+	assert.Equal(t, 1, pokerHands([]string{"3S AD 4D 5C 2C 2C 6H QD 5D 9C", "9C JD 4D 5C 2C KC 6H 4D 5D 8S"}))
+	assert.Equal(t, 2, pokerHands([]string{"3S AD 4D 5C 2C 2C 6H QD 5D 9C", "3S AD 4D 5C 2C 2C 6H QD 5D 9C"}))
+}
