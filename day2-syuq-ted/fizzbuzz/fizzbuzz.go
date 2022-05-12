a545a499a121d7ba29e976710c45e2964270b4c2
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-12 10:49:37 +0800

    tcring

--- day2-syuq-ted/fizzbuzz/fizzbuzz.go
+++ day2-syuq-ted/fizzbuzz/fizzbuzz.go
@@ -3,10 +3,10 @@ package fizzbuzz
 import "strconv"
 
 func fizzbuzz(number int) string {
-	if number%15 == 0 {
-		return "FIZZBUZZ"
-	}
 	if number%3 == 0 {
+		if number%15 == 0 {
+			return "FIZZBUZZ"
+		}
 		return "FIZZ"
 	}
 	if number%5 == 0 {
