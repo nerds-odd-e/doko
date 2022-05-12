91d3c03b10722dceadf4dd66d212c920493d0d20
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-12 16:07:00 +0800

    tcring

--- day2-syuq-ted/pokerHands/pokerHands.go
+++ day2-syuq-ted/pokerHands/pokerHands.go
@@ -4,6 +4,8 @@ import "strings"
 
 type Game []string
 
+type Card string
+
 func pokerHands(hands []string) int {
 	winCount := 0
 	for i := range hands {
@@ -16,16 +18,20 @@ func pokerHands(hands []string) int {
 }
 func (game Game) p1Wins() bool {
 	left, right := 4, 9
-	for game[left] == game[right] && left > 0 && right > 5 {
+	p1Card := Card(game[left])
+	p2Card := Card(game[right])
+	for p1Card == p2Card && left > 0 && right > 5 {
 		left -= 1
 		right -= 1
+		p1Card = Card(game[left])
+		p2Card = Card(game[right])
 	}
-	if string(game[right][0]) == "T" {
+	if string(p2Card[0]) == "T" {
 		return true
 	}
-	if string(game[left][0]) == "T" || string(game[left][0]) == "Q" {
+	if string(p1Card[0]) == "T" || string(p1Card[0]) == "Q" {
 		return false
 	}
 
-	return game[left] > game[right]
+	return p1Card > p2Card
 }
