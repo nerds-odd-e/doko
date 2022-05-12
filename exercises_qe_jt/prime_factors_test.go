4953bec382ecee411ca627faf17884a0cd5cb34e
Qing En <qingen@hotmail.sg>
2022-05-12 10:47:30 +0800

    tcring

--- exercises_qe_jt/prime_factors_test.go
+++ exercises_qe_jt/prime_factors_test.go
@@ -1,3 +1,15 @@
 package exercises_qe_jt
 
+import (
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+)
+
+func PrimeFactors(n int) string {
+	return ""
+}
+
+func TestPrimeFactors2(t * testing.T) {
+	assert.Equal(t, "", PrimeFactors(2))
+}
