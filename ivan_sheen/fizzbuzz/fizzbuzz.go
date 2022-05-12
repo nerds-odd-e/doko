7859aa1c6a88bc4a445051c129f404ea5974aded
tedmund + ivan <foo@bar.com>
2022-05-12 10:47:13 +0800

    tcring

--- ivan_sheen/fizzbuzz/fizzbuzz.go
+++ ivan_sheen/fizzbuzz/fizzbuzz.go
@@ -3,11 +3,15 @@ package ivan_sheen
 import "fmt"
 
 func fizzbuzz(n int) string {
-	if n%5 == 0 {
+	if isDivisibleBy(n, 5) {
 		return "buzz"
 	}
 	if n%3 == 0 {
 		return "fizz"
 	}
 	return fmt.Sprint(n)
+}
+
+func isDivisibleBy(n int, divisor int) bool {
+	return n%divisor == 0
 }
