6f68ae764bb541292f97da60d7b9d9e3af853aad
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-12 10:38:46 +0800

    tcring

--- day2-syuq-ted/fizzbuzz/fizzbuzz.go
+++ day2-syuq-ted/fizzbuzz/fizzbuzz.go
@@ -1,11 +1,10 @@
 package fizzbuzz
 
+import "strconv"
+
 func fizzbuzz(number int) string {
 	if number%3 == 0 {
 		return "FIZZ"
 	}
-	if number == 2 {
-		return "2"
-	}
-	return "1"
+	return strconv.Itoa(number)
 }
