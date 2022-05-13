4e607e6691241b7e033e208f8af8e42d928a95d3
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-13 11:46:52 +0800

    tcring

--- final_day_poker_hands/multiple_games_test.go
+++ final_day_poker_hands/multiple_games_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func makeP1WinHighestCardHighCardTwoGames() []string {
-	return []string{"2S 3C 4D 5H JH 2S 3D 4C 5H 6D", "2S 3C 4D 5H JH 2S 3D 4C 5H 6D"}
+	return []string{"2S 3C 4D 5H 9H 2S 3D 4C 5H 6D", "2S 3C 4D 5H 9H 2S 3D 4C 5H 6D"}
 }
 
 func TestP1WinsHighCardTwiceOfTwoGames(t *testing.T) {
