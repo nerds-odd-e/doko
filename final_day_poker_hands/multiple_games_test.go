527ec2ba4fb7af56d42466ab60cba9ed7fb843e0
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-13 11:30:02 +0800

    tcring

--- final_day_poker_hands/multiple_games_test.go
+++ final_day_poker_hands/multiple_games_test.go
@@ -1,11 +1,15 @@
 package final_day_poker_hands
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func makeP1WinHighestCardHighCardTwoGames() []string {
 	return []string{"2S 3C 4D 5H JH 2S 3D 4C 5H 6D", "2S 3C 4D 5H JH 2S 3D 4C 5H 6D"}
 }
 
 func TestP1WinsHighCardTwiceOfTwoGames(t *testing.T) {
-
+	assert.Equal(t, 1, pokerhands(makeP1WinHighestCardHighCardTwoGames()))
 }
