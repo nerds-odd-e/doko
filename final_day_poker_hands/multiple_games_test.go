104bfccb41007edd0788ad27d429b1ab20a584be
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-13 11:28:55 +0800

    tcring

--- final_day_poker_hands/multiple_games_test.go
+++ final_day_poker_hands/multiple_games_test.go
@@ -2,6 +2,10 @@ package final_day_poker_hands
 
 import "testing"
 
+func makeP1WinHighestCardHighCardTwoGames() []string {
+	return []string{"2S 3C 4D 5H JH 2S 3D 4C 5H 6D", "2S 3C 4D 5H JH 2S 3D 4C 5H 6D"}
+}
+
 func TestP1WinsHighCardTwiceOfTwoGames(t *testing.T) {
 
 }
