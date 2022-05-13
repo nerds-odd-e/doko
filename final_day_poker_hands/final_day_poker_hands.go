45a3eef10036e927c0314b8422414ae582ec12fb
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-13 11:31:45 +0800

    tcring

--- final_day_poker_hands/final_day_poker_hands.go
+++ final_day_poker_hands/final_day_poker_hands.go
@@ -2,14 +2,12 @@ package final_day_poker_hands
 
 import "strings"
 
-
-
 type Card string
 type Cards []Card
 
 func pokerhands(games []string) int {
 	if len(games) > 0 {
-		cards := strings.Split(games[0]," ")
+		cards := strings.Split(games[0], " ")
 		if cards[4] > cards[9] {
 			return 1
 		}
