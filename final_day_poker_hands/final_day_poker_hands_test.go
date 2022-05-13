ad6b5eb00c98eebad88938c2f3f1f616e225a27f
Tedmund Tan <tedtanzp@outlook.com>
2022-05-13 11:00:40 +0800

    tcring

--- final_day_poker_hands/final_day_poker_hands_test.go
+++ final_day_poker_hands/final_day_poker_hands_test.go
@@ -14,7 +14,7 @@ func makeP1HighestCardHighCard() string {
 	return "2S 3C 4D 5H 9H 2S 3D 4C 5H 6D"
 }
 
-func xTestReturn1IfP1HasHighestCardIfHandIsSorted(t *testing.T) {
+func TestReturn1IfP1HasHighestCardIfHandIsSorted(t *testing.T) {
 	assert.Equal(t, 1, pokerhands([]string{makeP1HighestCardHighCard()}))
 }
 
@@ -25,4 +25,3 @@ func makeP2HighestCardHighCard() string {
 func xTestReturn0IfP2HasHighestCardIfHandIsSorted(t *testing.T) {
 	assert.Equal(t, 0, pokerhands([]string{makeP2HighestCardHighCard()}))
 }
-
