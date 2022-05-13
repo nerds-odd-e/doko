593f0148e6fcd3df6e63401b2a82bf4f96979b68
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-05-13 10:39:53 +0800

    tcring

--- shell.nix
+++ shell.nix
@@ -59,7 +59,8 @@ in mkShell {
     xvfb-run
   ];
   shellHook = ''
-    #!${pkgs.bash}
+    #!/bin/bash
+    bash --posix
     export DOKO_ROOT=$PWD
     . $DOKO_ROOT/scripts/durable-storage-utils.sh
 
