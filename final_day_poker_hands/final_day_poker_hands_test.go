10d2ff2d8116e177f7788431d43181a169426ef6
Qing En <qingen@hotmail.sg>
2022-05-13 10:47:52 +0800

    tcring

--- final_day_poker_hands/final_day_poker_hands_test.go
+++ final_day_poker_hands/final_day_poker_hands_test.go
@@ -10,6 +10,7 @@ func TestReturn0WinsIfInputIsEmpty(t *testing.T) {
 	assert.Equal(t, 0, pokerhands([]string{}))
 }
 
-func TestReturn1WinIfP1HasHighestCard(t *testing.T) {
 
+func TestReturn1IfP1HasHighestCard(t *testing.T){
+	assert.Equal(t,0,pokerhands([]string{"8C TS KC 9H AS 4S 7D 2S 5D 3S"}))
 }
