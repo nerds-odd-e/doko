9047c6a8963726a6f5aa66f4581acec835e25a03
Qing En <qingen@hotmail.sg>
2022-05-13 11:23:20 +0800

    tcring

--- final_day_poker_hands/final_day_poker_hands_test.go
+++ final_day_poker_hands/final_day_poker_hands_test.go
@@ -28,28 +28,3 @@ func TestReturn0IfP2HasHighestCardIfHandIsSorted(t *testing.T) {
 	assert.Equal(t, 0, pokerhands([]string{makeP2HighestCardHighCard()}))
 }
 
-func makeP1HighCardWinBySuite() string {
-	return "2S 3C 4D 5H 9S 2S 3D 4C 5H 9H"
-}
-
-func xTestReturn1IfP1WinBySuiteIfHandIsSorted(t *testing.T) {
-	assert.Equal(t, 1, pokerhands([]string{makeP1HighCardWinBySuite()}))
-}
-
-
-func makeP2HighCardWinBySuite() string {
-	return "2S 3D 4C 5H 9H 2S 3C 4D 5H 9S"
-}
-
-func xTestReturn0IfP2WinBySuiteIfHandIsSorted(t *testing.T){
-	assert.Equal(t,0,pokerhands([]string{makeP2HighCardWinBySuite()}))
-}
-
-
-func xTestReturn1IfP1HasHighestCardAbove10IfHandIsSorted(t *testing.T) {
-	assert.Equal(t, 1, pokerhands([]string{makeP1HighestCardHighCardWithJack()}))
-}
-
-func makeP1HighestCardHighCardWithJack() string {
-	return "2S 3C 4D 5H JH 2S 3D 4C 5H 6D"
-}
