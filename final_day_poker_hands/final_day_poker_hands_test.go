a7351b882efde08e43681d178eb3812fc3a8e655
tedmund + ivan <foo@bar.com>
2022-05-13 11:05:43 +0800

    tcring

--- final_day_poker_hands/final_day_poker_hands_test.go
+++ final_day_poker_hands/final_day_poker_hands_test.go
@@ -44,3 +44,11 @@ func xTestReturn0IfP2WinBySuiteIfHandIsSorted(t *testing.T){
 	assert.Equal(t,0,pokerhands([]string{makeP2HighCardWinBySuite()}))
 }
 
+
+func xTestReturn1IfP1HasHighestCardAbove10IfHandIsSorted(t *testing.T) {
+	assert.Equal(t, 1, pokerhands([]string{makeP1HighestCardHighCardWithJack()}))
+}
+
+func makeP1HighestCardHighCardWithJack() string {
+	return "2S 3C 4D 5H JH 2S 3D 4C 5H 6D"
+}
