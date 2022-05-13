e224c12a52ef29b97a7cf6ef7a74385445d51599
Qing En <qingen@hotmail.sg>
2022-05-13 11:27:20 +0800

    tcring

--- final_day_poker_hands/final_day_poker_hands.go
+++ final_day_poker_hands/final_day_poker_hands.go
@@ -1,12 +1,10 @@
 package final_day_poker_hands
 
 func pokerhands(game []string) int {
-	if len(game) == 0 {
-		return 0
-	}
-
-	if game[0][12] > game[0][27] {
-		return 1
+	if len(game) > 0 {
+		if game[0][12] > game[0][27] {
+			return 1
+		}
 	}
 	return 0
 }
