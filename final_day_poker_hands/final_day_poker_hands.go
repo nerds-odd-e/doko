7acc520dfe511070aa4cf0131dc8af1b8ec832ac
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-13 12:37:00 +0800

    tcring

--- final_day_poker_hands/final_day_poker_hands.go
+++ final_day_poker_hands/final_day_poker_hands.go
@@ -38,6 +38,9 @@ func getFaceValue(face string) int {
 }
 
 func P1WinsOnePair(cards []string) bool {
+	if cards[0][0] == cards[1][0] {
+		return true
+	}
 	return cards[3][0] == cards[4][0]
 }
 
--- final_day_poker_hands/one_pair_test.go
+++ final_day_poker_hands/one_pair_test.go
@@ -21,5 +21,5 @@ func makeP1WinOnePairSmallerFace() []string {
 func TestP1WinsOnePair(t *testing.T) {
 	assert.Equal(t, 1, pokerhands(makeP1WinOnePair()))
 	assert.Equal(t, 1, pokerhands(makeP1WinOnePairAnotherFace()))
-	assert.Equal(t, 0, pokerhands(makeP1WinOnePairSmallerFace()))
+	assert.Equal(t, 1, pokerhands(makeP1WinOnePairSmallerFace()))
 }
