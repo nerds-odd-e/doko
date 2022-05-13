f8de858c3f24d2c1cd425be6a2c72f6a9e383f52
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-13 10:53:31 +0800

    tcring

--- final_day_poker_hands/final_day_poker_hands_test.go
+++ final_day_poker_hands/final_day_poker_hands_test.go
@@ -15,5 +15,5 @@ func makeP1HighestCardHighCard() string {
 }
 
 func xTestReturn1IfP1HasHighestCardAtPosition5(t *testing.T) {
-	assert.Equal(t, 1, pokerhands([]string{"8C TS KC 9H AS 4S 7D 2S 5D 3S"}))
+	assert.Equal(t, 1, pokerhands([]string{"8C TS KC 7H 9S 4S 7D 2S 5D 3S"}))
 }
