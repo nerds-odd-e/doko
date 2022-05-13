0f03c63b5c853df98f898e36d2a60385f0731d5b
Qing En <qingen@hotmail.sg>
2022-05-13 15:00:43 +0800

    tcring

--- final_day_poker_hands/final_day_poker_hands.go
+++ final_day_poker_hands/final_day_poker_hands.go
@@ -56,7 +56,7 @@ func getFaceValue(face string) int {
 	return faceValue
 }
 
-func (hand1 Hand) P1WinsOnePair() bool {
+func (hand1 Hand) WinsByOnePair() bool {
 	for i := 0; i < 4; i++ {
 		if hand1[i][0] == hand1[i+1][0] {
 			return true
@@ -65,7 +65,7 @@ func (hand1 Hand) P1WinsOnePair() bool {
 	return false
 }
 
-func (hand1 Hand) P1WinsByHighCard(hand2 Hand) bool {
+func (hand1 Hand) WinsByHighCard(hand2 Hand) bool {
 	for i := 4; i >= 0; i-- {
 		if hand1[i][:1] != hand2[i][:1] {
 			return getFaceValue(hand1[i][:1]) > getFaceValue(hand2[i][:1])
@@ -77,7 +77,7 @@ func (hand1 Hand) P1WinsByHighCard(hand2 Hand) bool {
 type Hand []string
 
 func (hand1 Hand) Wins(hand2 Hand) bool {
-	return hand1.P1WinsOnePair() || hand1.P1WinsByHighCard(hand2)
+	return hand1.WinsByOnePair() || hand1.WinsByHighCard(hand2)
 }
 
 func (h Hand) sort() Hand {
