a3dcd351a8a9c7ff483c201009f7b425a3d21ae1
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-13 11:35:04 +0800

    tcring

--- final_day_poker_hands/final_day_poker_hands.go
+++ final_day_poker_hands/final_day_poker_hands.go
@@ -6,15 +6,12 @@ type Card string
 type Cards []Card
 
 func pokerhands(games []string) int {
-	if len(games) > 0 {
-		p1WinCount := 0
-		for _, game := range games {
-			cards := strings.Split(game, " ")
-			if cards[4] > cards[9] {
-				p1WinCount += 1
-			}
+	p1WinCount := 0
+	for _, game := range games {
+		cards := strings.Split(game, " ")
+		if cards[4] > cards[9] {
+			p1WinCount += 1
 		}
-		return p1WinCount
 	}
-	return 0
+	return p1WinCount
 }
