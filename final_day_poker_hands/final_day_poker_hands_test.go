4d289d827621ba565d0a369917109bdda064c4da
Qing En <qingen@hotmail.sg>
2022-05-13 10:57:06 +0800

    tcring

--- final_day_poker_hands/final_day_poker_hands_test.go
+++ final_day_poker_hands/final_day_poker_hands_test.go
@@ -11,13 +11,17 @@ func TestReturn0WinsIfInputIsEmpty(t *testing.T) {
 }
 
 func makeP1HighestCardHighCard() string {
-	return "2S 3C 4D 5H 9H 6D 2S 3D 4C 5H"
+	return "2S 3C 4D 5H 9H 2S 3D 4C 5H 6D"
 }
 
 func xTestReturn1IfP1HasHighestCardIfHandIsSorted(t *testing.T){
 	assert.Equal(t,1,pokerhands([]string{makeP1HighestCardHighCard()}))
 }
 
+func makeP2HighestCardHighCard() string {
+	return "2S 3D 4C 5H 6D 2S 3C 4D 5H 9H"
+}
+
 func xTestReturn0IfP2HasHighestCardIfHandIsSorted(t *testing.T){
-	assert.Equal(t,0,pokerhands([]string{"2C 3H 4C 5C 6C 2S 3S 4S 5S AS"}))
+	assert.Equal(t,0,pokerhands([]string{makeP2HighestCardHighCard()}))
 }
