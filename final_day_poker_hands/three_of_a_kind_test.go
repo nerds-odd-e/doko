238882e22d90ffa45509a67bedc2942945f80a2f
Tedmund Tan <tedtanzp@outlook.com>
2022-05-13 14:46:48 +0800

    tcring

--- final_day_poker_hands/three_of_a_kind_test.go
+++ final_day_poker_hands/three_of_a_kind_test.go
@@ -10,6 +10,11 @@ func makeP1WinThreeOfAKind() []string {
 	return []string{"2S 3C 8D 8S 8H 2S 3D 4C 5H 9H"}
 }
 
+func makeP2WinThreeOfAKind() []string {
+	return []string{"2S 3C 4D 5S 8H 2S 3D 9C 9H 9H"}
+}
+
 func TestReturn1IfP1HasThreeOfAKind(t *testing.T) {
 	assert.Equal(t, 1, pokerhands(makeP1WinThreeOfAKind()))
+	assert.Equal(t, 0, pokerhands(makeP2WinThreeOfAKind()))
 }
