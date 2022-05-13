e164878bd342d0dcd809623cdcd02fdebc42ce8c
Tedmund Tan <tedtanzp@outlook.com>
2022-05-13 12:40:46 +0800

    tcring

--- final_day_poker_hands/three_of_a_kind_test.go
+++ final_day_poker_hands/three_of_a_kind_test.go
@@ -6,6 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func makeP1WinThreeOfAKind() []string {
+	return []string{"2S 3C 8D 8S 8H 2S 3D 4C 5H 9H"}
+}
+
 func TestReturn1IfP1HasThreeOfAKind(t *testing.T) {
 	assert.Equal(t, true, true)
 }
