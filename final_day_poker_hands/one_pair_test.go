35c526a5df50f5037abf3906e520aacbbcede66e
Syuqri <muhammad_syuqri@tech.gov.sg>
2022-05-13 12:14:06 +0800

    tcring

---
+++ final_day_poker_hands/one_pair_test.go
@@ -0,0 +1,15 @@
+package final_day_poker_hands
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func makeP1WinOnePair() []string {
+	return []string{"2S 3C 4D 8S 8H 2S 3D 4C 5H 9H"}
+}
+
+func TestP1WinsOnePair(t *testing.T) {
+	assert.Equal(t, 0, pokerhands(makeP1WinOnePair()))
+}
