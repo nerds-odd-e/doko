9af15e2f06eda08589d2e0cd297b6c3c8827baac
puree.kla1997 <puree.kla@snocko-tech.com>
2022-07-14 17:20:23 +0700

    tcring

--- BankSun/Poker/main.go
+++ BankSun/Poker/main.go
@@ -23,7 +23,6 @@ func calculatorPercentWinnerPoker(pokerFile []string) int {
 	if len(pokerFile) == 0 {
 		return 0
 	}
-	findHighest(pokerFile)
 	var score int
 	for _, v := range pokerFile {
 		pokerHands := strings.Split(v, "    ")
@@ -39,8 +38,4 @@ func (game PokerGame) compareHand() bool {
 		return false
 	}
 	return game.hand1[0] == 'A' || game.hand1[0] > game.hand2[0] || (game.hand1[0] == 'K' && game.hand2[0] == 'Q') || (game.hand1[0] == 'J' && game.hand2[0] == 'T')
-}
-
-func findHighest(hand []string) []string {
-	return hand
 }
