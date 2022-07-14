3bc34d05ea9aa59083dc411fa45e789c30339ed7
chayutdoo <chayut.doo@snocko-tech.com>
2022-07-14 15:48:15 +0700

    tcring

--- MeenNew/round.go
+++ MeenNew/round.go
@@ -10,7 +10,8 @@ func (r *Round) isPlayer1Winner() bool {
 	sortRank := []string{"A", "K", "Q"}
 	for _, rank := range sortRank {
 		founded := strings.Index(r.value, rank)
-		if founded < 14 && founded >= 0 {
+		foundAtPlayer1 := founded < 14 && founded >= 0
+		if foundAtPlayer1 {
 			return true
 		}
 		if founded >= 14 {
