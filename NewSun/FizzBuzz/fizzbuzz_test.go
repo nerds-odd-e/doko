e99998ea743b937a3000eac56e051d92223ce7db
thotsakorn.sir <thotsakorn.sir@snocko-tech.com>
2022-07-14 10:31:22 +0700

    tcring

--- NewSun/FizzBuzz/fizzbuzz_test.go
+++ NewSun/FizzBuzz/fizzbuzz_test.go
@@ -19,6 +19,12 @@ func Test_FizzbuzzInput2(t *testing.T) {
 	assert.Equal(t, a, b)
 }
 
+func Test_FizzbuzzInput3(t *testing.T) {
+	a := inputFizzbuzz(3)
+	b := "3"
+	assert.Equal(t, a, b)
+}
+
 func inputFizzbuzz(input int64) string {
 	return fmt.Sprint(input)
 }
