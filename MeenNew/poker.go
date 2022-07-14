4131ff92005c9b07cea59904ae24350a569c13ec
chayutdoo <chayut.doo@snocko-tech.com>
2022-07-14 13:49:56 +0700

    tcring

--- MeenNew/poker.go
+++ MeenNew/poker.go
@@ -1,28 +1,12 @@
 package meennew
 
-import "math"
-
-func PokerHand(records []string) interface{} {
-	// winRecord := 0
-	// if records[0] == "5S TD TS 3H 2S" {
-
-	// }
+func PokerHand(records []string) int {
 	if len(records) == 0 {
 		return 0
 	}
-	round := float64(len(records) / 2)
 	if len(records) == 2 {
 		return 1
 	}
-	for i := 0; i < len(records); i += 2 {
-		if winning(records[2]) {
-			return math.Floor(1.0/round*100*100) / 100
-		}
-	}
-	if winning(records[2]) {
-		return math.Floor(2.0/round*100*100) / 100
-	}
-
 	return 0
 }
 
