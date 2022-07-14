bd5ab56abb075b9c270d277cc1defe30eb0fa558
thotsakorn.sir <thotsakorn.sir@snocko-tech.com>
2022-07-14 15:38:13 +0700

    tcring

--- DaiWin/pokerhand/pokerhand_test.go
+++ DaiWin/pokerhand/pokerhand_test.go
@@ -18,64 +18,64 @@ const highCardWithSeven = "2H 3H 4H 5H 7S"
 
 func TestPlayer1Win0TimesWhenNoGamePlayed(t *testing.T) {
 	pokerFile := []string{}
-	assert.Equal(t, 0, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 0, CalculatePlayer1WinGame(pokerFile))
 }
 
 func TestPlayer1WinOnceIn1Game(t *testing.T) {
 	pokerFile := []string{highCardWithEigth + "  " + highCardWithSeven}
-	assert.Equal(t, 1, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 1, CalculatePlayer1WinGame(pokerFile))
 }
 func TestPlayer1WinOnceIn1Game1(t *testing.T) {
 	pokerFile := []string{highCardWithNine + "  " + highCardWithSeven}
-	assert.Equal(t, 1, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 1, CalculatePlayer1WinGame(pokerFile))
 }
 
 func TestPlayer1WinOnceIn2Game(t *testing.T) {
 	pokerFile := []string{highCardWithEigth + "  " + highCardWithSeven, highCardWithSeven + "  " + highCardWithEigth}
-	assert.Equal(t, 1, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 1, CalculatePlayer1WinGame(pokerFile))
 }
 
 func TestPlayer1LoseOnceIn1Game(t *testing.T) {
 	pokerFile := []string{highCardWithSeven + "  " + highCardWithEigth}
-	assert.Equal(t, 0, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 0, CalculatePlayer1WinGame(pokerFile))
 }
 
 func TestPlayer1LoseOnceIn2Game(t *testing.T) {
 	pokerFile := []string{highCardWithSeven + "  " + highCardWithEigth, highCardWithEigth + "  " + highCardWithSeven}
-	assert.Equal(t, 1, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 1, CalculatePlayer1WinGame(pokerFile))
 }
 
 func TestPlayer1Win2In2Game(t *testing.T) {
 	pokerFile := []string{highCardWithEigth + "  " + highCardWithSeven, highCardWithEigth + "  " + highCardWithSeven}
-	assert.Equal(t, 2, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 2, CalculatePlayer1WinGame(pokerFile))
 }
 
 func TestPlayer1WinOnceUn1GameWithKingCard(t *testing.T) {
 	pokerFile := []string{highCardWithKing + "  " + highCardWithSeven}
-	assert.Equal(t, 1, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 1, CalculatePlayer1WinGame(pokerFile))
 }
 
 func TestPlayer1WinOnceUn1GameWithAceCard(t *testing.T) {
 	pokerFile := []string{highCardWithAce + "  " + highCardWithKing}
-	assert.Equal(t, 1, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 1, CalculatePlayer1WinGame(pokerFile))
 }
 
 func TestPlayer1WinOnceUn1GameWithQueenCard(t *testing.T) {
 	pokerFile := []string{highCardWithQueen + "  " + highCardWithJack}
-	assert.Equal(t, 1, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 1, CalculatePlayer1WinGame(pokerFile))
 }
 
 func TestPlayer1WinOnceUn1GameWithJackCard(t *testing.T) {
 	pokerFile := []string{highCardWithJack + "  " + highCardWithTen}
-	assert.Equal(t, 1, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 1, CalculatePlayer1WinGame(pokerFile))
 }
 
 func TestPlayer1WinOnceUn1GameWithTenCard(t *testing.T) {
 	pokerFile := []string{highCardWithTen + "  " + highCardWithNine}
-	assert.Equal(t, 1, ValidatePokerFile(pokerFile))
+	assert.Equal(t, 1, CalculatePlayer1WinGame(pokerFile))
 }
 
-func ValidatePokerFile(list []string) int {
+func CalculatePlayer1WinGame(list []string) int {
 	player1WinningCount := 0
 	for _, row := range list {
 		cardInHand := strings.Split(row, "  ")
