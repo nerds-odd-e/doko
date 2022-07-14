e2c5b0f386a1a70e097a39885f5bf650486d3cfc
chayutdoo <chayut.doo@snocko-tech.com>
2022-07-14 17:42:47 +0700

    tcring

--- MeenNew/round_test.go
+++ MeenNew/round_test.go
@@ -52,6 +52,6 @@ func TestP1DrawWithTen(t *testing.T) {
 }
 
 func TestP1Twopair(t *testing.T) {
-	round := Round{value: "2C TD TS 3H 2S 4C 2H 5H 8H 9H"}
+	round := Round{value: "2C TD 5S 3H 2S 4C 2H 5H TH 9H"}
 	assert.Equal(t, round.isPlayer1Winner(), false)
 }
