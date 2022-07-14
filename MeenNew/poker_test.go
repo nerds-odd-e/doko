1bfc367f29d6b1c0632ea5cd05c35c10f5c494c4
thotsakorn.sir <thotsakorn.sir@snocko-tech.com>
2022-07-14 10:22:11 +0700

    tcring

--- MeenNew/poker_test.go
+++ MeenNew/poker_test.go
@@ -18,20 +18,20 @@ func TestOneRecord(t *testing.T) {
 	assert.Equal(t, winRateP1, 100.0)
 }
 
-func TestTwoRecord(t *testing.T) {
-	records := []string{"5S TD TS 3H 2S", "4H 2H AH 6S QS", "5S TD TS 3H 2S", "4H 2H AH 6S QS"}
-	winRateP1 := PokerHand(records)
-	assert.Equal(t, winRateP1, 100.0)
-}
+// func TestTwoRecord(t *testing.T) {
+// 	records := []string{"5S TD TS 3H 2S", "4H 2H AH 6S QS", "5S TD TS 3H 2S", "4H 2H AH 6S QS"}
+// 	winRateP1 := PokerHand(records)
+// 	assert.Equal(t, winRateP1, 100.0)
+// }
 
-func TestThreeRecord(t *testing.T) {
-	records := []string{"5S TD TS 3H 2S", "4H 2H AH 6S QS", "5S TD TS 3H 2S", "4H 2H AH 6S QS", "4H 2H AH 6S QS", "5S TD TS 3H 2S"}
-	winRateP1 := PokerHand(records)
-	assert.Equal(t, winRateP1, 66.66)
-}
+// func TestThreeRecord(t *testing.T) {
+// 	records := []string{"5S TD TS 3H 2S", "4H 2H AH 6S QS", "5S TD TS 3H 2S", "4H 2H AH 6S QS", "4H 2H AH 6S QS", "5S TD TS 3H 2S"}
+// 	winRateP1 := PokerHand(records)
+// 	assert.Equal(t, winRateP1, 66.66)
+// }
 
-func TestThreeRecordP1OneWin(t *testing.T) {
-	records := []string{"5S TD TS 3H 2S", "4H 2H AH 6S QS", "4H 2H AH 6S QS", "5S TD TS 3H 2S", "4H 2H AH 6S QS", "5S TD TS 3H 2S"}
-	winRateP1 := PokerHand(records)
-	assert.Equal(t, winRateP1, 33.33)
-}
+// func TestThreeRecordP1OneWin(t *testing.T) {
+// 	records := []string{"5S TD TS 3H 2S", "4H 2H AH 6S QS", "4H 2H AH 6S QS", "5S TD TS 3H 2S", "4H 2H AH 6S QS", "5S TD TS 3H 2S"}
+// 	winRateP1 := PokerHand(records)
+// 	assert.Equal(t, winRateP1, 33.33)
+// }
