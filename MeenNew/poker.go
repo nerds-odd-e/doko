86c1fbd5d244ea3347134631a8ec6446791a8786
chayutdoo <chayut.doo@snocko-tech.com>
2022-07-14 14:03:07 +0700

    tcring

--- MeenNew/poker.go
+++ MeenNew/poker.go
@@ -1,14 +1,5 @@
 package meennew
 
 func PokerHand(records []string) int {
-	if len(records) == 0 {
-		return len(records)
-	}
-	if len(records) == 1 {
-		return len(records)
-	}
-	if len(records) == 2 {
-		return len(records)
-	}
 	return len(records)
 }
