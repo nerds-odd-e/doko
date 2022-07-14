86815e0e0c1378930745298081750f872baca1d6
chayutdoo <chayut.doo@snocko-tech.com>
2022-07-14 15:39:22 +0700

    tcring

--- MeenNew/poker.go
+++ MeenNew/poker.go
@@ -12,18 +12,18 @@ func PokerHand(records []string) int {
 	if len(records) == 0 {
 		return countP1Winner
 	}
-	for _, v := range records {
-		if isPlayer1Winner(v) {
+	for _, value := range records {
+		if isPlayer1Winner(Round{value}) {
 			countP1Winner += 1
 		}
 	}
 	return countP1Winner
 }
 
-func isPlayer1Winner(round string) bool {
+func isPlayer1Winner(round Round) bool {
 	sortRank := []string{"A", "K", "Q"}
 	for _, rank := range sortRank {
-		hasQueen := strings.Index(round, rank)
+		hasQueen := strings.Index(round.value, rank)
 		if hasQueen < 14 && hasQueen >= 0 {
 			return true
 		}
