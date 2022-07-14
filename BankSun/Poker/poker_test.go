2aa464db74ebf9a4cebe4b584602994c0a88fe84
puree.kla1997 <puree.kla@snocko-tech.com>
2022-07-14 14:56:53 +0700

    tcring

--- BankSun/Poker/poker_test.go
+++ BankSun/Poker/poker_test.go
@@ -199,5 +199,5 @@ func TestP1WinByHighcard(t *testing.T) {
 }
 
 func TestPlayer1LostByHighCard(t *testing.T) {
-	assert.Equal(t, calculatorPercentWinnerPoker(aGameOfTwoHighCards("K", "A")), 1)
+	assert.Equal(t, calculatorPercentWinnerPoker(aGameOfTwoHighCards("K", "A")), 0)
 }
