a1e78fa88a845ae122968509825d6f92ed63c8da
thotsakorn.sir <thotsakorn.sir@snocko-tech.com>
2022-07-14 10:51:45 +0700

    tcring

--- NewSun/FizzBuzz/fizzbuzz_test.go
+++ NewSun/FizzBuzz/fizzbuzz_test.go
@@ -63,7 +63,7 @@ func inputFizzbuzz(input int64) string {
 	if input%3 == 0 && input%5 == 0 {
 		return "FizzBuzz"
 	}
-	if input%3 == 0 {
+	if input%3 == 0  {
 		return "Fizz"
 	}
 	if input%5 == 0 {
