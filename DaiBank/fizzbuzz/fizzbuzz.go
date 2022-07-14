2e7d9f2cf3e1f4cc61e42d05a9317b91edf3ef9d
chayutdoo <chayut.doo@snocko-tech.com>
2022-07-14 10:49:42 +0700

    tcring

--- DaiBank/fizzbuzz/fizzbuzz.go
+++ DaiBank/fizzbuzz/fizzbuzz.go
@@ -4,14 +4,14 @@ package main
 import "fmt"
 
 func findFizzBuzz(num int) string {
-	if num == 3 {
-		return "Fizz"
+	if num == 15 {
+		return "FizzBuzz"
 	}
 	if num == 5 {
 		return "Buzz"
 	}
-	if num == 15 {
-		return "FizzBuzz"
+	if num%3 == 0 {
+		return "Fizz"
 	}
 	return fmt.Sprint(num)
 }
--- DaiBank/fizzbuzz/fizzbuzz_test.go
+++ DaiBank/fizzbuzz/fizzbuzz_test.go
@@ -10,11 +10,11 @@ func TestNormalNumber(t *testing.T) {
 	assert.Equal(t, "1", findFizzBuzz(1))
 	assert.Equal(t, "2", findFizzBuzz(2))
 	assert.Equal(t, "4", findFizzBuzz(4))
-	assert.Equal(t, "6", findFizzBuzz(6))
 }
 
 func TestFizz(t *testing.T) {
 	assert.Equal(t, "Fizz", findFizzBuzz(3))
+	assert.Equal(t, "Fizz", findFizzBuzz(6))
 }
 
 func TestBuzz(t *testing.T) {
