587d6c6e011d347519e1e695281a34834008eea7
chayutdoo <chayut.doo@snocko-tech.com>
2022-07-14 14:05:24 +0700

    tcring

--- MeenNew/poker_test.go
+++ MeenNew/poker_test.go
@@ -29,8 +29,9 @@ func TestTwoRecord(t *testing.T) {
 
 func TestPlayerOneWin1Time(t *testing.T) {
 	records := []string{
+		"5S 6D 7S 8H 9S 4H 2H AH 6S QS",
 		"5S TD TS 3H 2S 4H 2H AH 6S QS",
 	}
 	winRateP1 := PokerHand(records)
-	assert.Equal(t, winRateP1, 1)
+	assert.Equal(t, winRateP1, 2)
 }
