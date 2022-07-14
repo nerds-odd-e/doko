02fc1b009700bcacf3cf10e1f27d2c27540a0499
puree.kla1997 <puree.kla@snocko-tech.com>
2022-07-14 14:32:04 +0700

    tcring

--- BankSun/Poker/poker_test.go
+++ BankSun/Poker/poker_test.go
@@ -172,7 +172,6 @@ func TestPlayerWin2in2Game(t *testing.T) {
 func TestPlayerWin1in2GameHighAK(t *testing.T) {
 	a := []string{
 		"AH 9C 9D AS 9H    KH 9C 9D AS 9H",
-		"8H 9C 9D AS 9H    9H 9C 9D AS 9H",
 	}
 	assert.Equal(t, calculatorPercentWinnerPoker(a), 1)
 }
