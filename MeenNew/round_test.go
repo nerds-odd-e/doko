a76499ca9cd85aca775053ea4ac89ccf51658abd
chayutdoo <chayut.doo@snocko-tech.com>
2022-07-14 16:09:45 +0700

    tcring

--- MeenNew/round_test.go
+++ MeenNew/round_test.go
@@ -15,3 +15,8 @@ func TestP1WinWithKing(t *testing.T) {
 	round := Round{value: "5S 9S KS 3H 2S 4H 2H 5H 6S 7S"}
 	assert.Equal(t, round.isPlayer1Winner(), true)
 }
+
+func TestP1WinWithQueen(t *testing.T) {
+	round := Round{value: "5S 9S QS 3H 2S 4H 2H 5H 6S 7S"}
+	assert.Equal(t, round.isPlayer1Winner(), true)
+}
