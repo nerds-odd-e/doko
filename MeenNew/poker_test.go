8ad089b3c4ba5969a6f978d55527bccba47fe6e3
chayutdoo <chayut.doo@snocko-tech.com>
2022-07-14 16:03:06 +0700

    tcring

--- MeenNew/poker_test.go
+++ MeenNew/poker_test.go
@@ -46,7 +46,7 @@ func TestPlayerOneWinInTwoRound(t *testing.T) {
 
 func TestPlayer2WinOf3Round(t *testing.T) {
 	records := []string{
-		"5S 6D TS 3H 2S 4H 2H 5H 6S 9S",
+		"5S 6D TS 3H 2S 4H 2H 5H 6S QS",
 		"5S AD TS 3H 2S 4H 2H 5H 6S 9S",
 		"5S AD TS 3H 2S 4H 2H 5H 6S 9S",
 	}
@@ -91,5 +91,5 @@ func TestCompareP1WinHighCardWithTen(t *testing.T) {
 		"5S 9S TS 3H 2S 4H 2H 5H 6S 7S",
 	}
 	winRateP1 := PokerHand(records)
-	assert.Equal(t, winRateP1, 0)
+	assert.Equal(t, winRateP1, 1)
 }
