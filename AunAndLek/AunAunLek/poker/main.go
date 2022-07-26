437db3daa291e8f19e740055a168349b1aea0f0d
Aunnop <aunnop@odds.team>
2022-07-26 15:04:59 +0700

    tcring

--- AunAndLek/AunAunLek/poker/main.go
+++ AunAndLek/AunAunLek/poker/main.go
@@ -1,11 +1,15 @@
 package main
 
+func IsP1Lose(input string) bool {
+	wincase := "3H 7H 6S KC JS QH TD JC 2D 8S"
+	return input != wincase
+}
+
 func Player1Win(input []string) int {
 	if len(input) == 0 {
 		return 0
 	}
-	wincase := "3H 7H 6S KC JS QH TD JC 2D 8S"
-	if input[0] != wincase {
+	if IsP1Lose(input[0]) {
 		return 0
 	}
 	return len(input)
