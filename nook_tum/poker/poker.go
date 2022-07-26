c518e734660eef0cd86957186c005cfadf76a015
sakkatos <sakkatos1980@hotmail.com>
2022-07-26 15:07:00 +0700

    tcring

--- nook_tum/poker/poker.go
+++ nook_tum/poker/poker.go
@@ -5,18 +5,18 @@ import (
 	"strings"
 )
 
-func IsPlayer1Win(cards string) bool {
+func IsPlayer1Win(cards string) string {
 	splitCards := strings.Split(cards, " ")
 	player1 := (splitCards[:5])
 	player2 := (splitCards[5:])
 	fmt.Printf("%v\n%v\n", player1, player2)
 	if player2[4][0] == 'A' {
 		// fmt.Printf("%v\n", player2[4])
-		return false
+		return "false"
 	}
 	if player2[3][0] == 'A' {
 		// fmt.Printf("%v\n", player2[3])
-		return false
+		return "false"
 	}
-	return true
+	return ""
 }
