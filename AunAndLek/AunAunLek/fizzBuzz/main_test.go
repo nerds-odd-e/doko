e8d2862e8a88c03c2eff8e8d358b43d093c04aa4
Aunnop <aunnop@odds.team>
2022-07-26 10:38:04 +0700

    tcring

--- AunAndLek/AunAunLek/fizzBuzz/main_test.go
+++ AunAndLek/AunAunLek/fizzBuzz/main_test.go
@@ -15,35 +15,15 @@ func TestFizzBuzz(t *testing.T) {
 	}
 	cases := []testcase{
 		{expected: "1", input: 1},
+		{expected: "2", input: 2},
+		{expected: "fizz", input: 3},
+		{expected: "buzz", input: 5},
+		{expected: "fizz", input: 6},
+		{expected: "buzz", input: 10},
 	}
 	for _, c := range cases {
 		t.Run(fmt.Sprintf("Input %v get '%v'", c.input, c.expected), func(t *testing.T) {
 			assert.Equal(t, c.expected, FizzBuzz(c.input))
 		})
 	}
-	expected := cases[0].expected
-	input := cases[0].input
-	t.Run(fmt.Sprintf("Input %v get '%v'", input, expected), func(t *testing.T) {
-		assert.Equal(t, expected, FizzBuzz(input))
-	})
-
-	t.Run("Input 2 get '2'", func(t *testing.T) {
-		assert.Equal(t, "2", FizzBuzz(2))
-	})
-
-	t.Run("Input 3 get 'fizz'", func(t *testing.T) {
-		assert.Equal(t, "fizz", FizzBuzz(3))
-	})
-
-	t.Run("Input 5 get 'buzz'", func(t *testing.T) {
-		assert.Equal(t, "buzz", FizzBuzz(5))
-	})
-
-	t.Run("Input 6 get 'fizz'", func(t *testing.T) {
-		assert.Equal(t, "fizz", FizzBuzz(6))
-	})
-
-	t.Run("Input 10 get 'buzz'", func(t *testing.T) {
-		assert.Equal(t, "buzz", FizzBuzz(10))
-	})
 }
