7ef58bc62d19916654c4af92c4a891dd3b3df9f5
sakkatos <sakkatos1980@hotmail.com>
2022-07-26 15:48:28 +0700

    tcring

--- nook_tum/poker/poker_test.go
+++ nook_tum/poker/poker_test.go
@@ -9,23 +9,25 @@ import (
 func Test_Player1LooseWithHighCard_Card5(t *testing.T) {
 	answer := IsPlayer1Win("8C TS KC 9H 4S 7D 2S 5D 4S AC")
 	expected := false
-	assert.Equal(t, answer, expected, "expected player1 loose get %v", answer)
+	assert.Equal(t, expected, answer, "expected player1 loose get %v", answer)
 }
 
 func Test_Player1LooseWithHighCard_Card4(t *testing.T) {
 	answer := IsPlayer1Win("8C TS KC 9H 4S 7D 2S 5D AS 6C")
 	expected := false
-	assert.Equal(t, answer, expected, "expected player1 loose get %v", answer)
+	assert.Equal(t, expected, answer, "expected player1 loose get %v", answer)
 }
 
 func Test_Player1WinWithHighCard_Card5(t *testing.T) {
 	answer := IsPlayer1Win("8C TS KC 9H AS 7D 2S 5D 4S 1C")
 	expected := true
-	assert.Equal(t, answer, expected, "expected player1 win get %v", answer)
+	assert.Equal(t, expected, answer, "expected player1 win get %v", answer)
 }
 
 func Test_Player1WinWithHighCard_Card4(t *testing.T) {
 	answer := IsPlayer1Win("8C TS KC AS 1S 7D 2S 5D 4S 1C")
 	expected := true
-	assert.Equal(t, answer, expected, "expected player1 win get %v", answer)
+	assert.Equal(t, expected, answer, "expected player1 win get %v", answer)
 }
+
+
