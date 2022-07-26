d2dea7c0ea5e62e067a6e149d4851ba2d5a21d17
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2022-07-26 15:43:02 +0700

    tcring

--- kelvin-o/pokerhand/pokerhand_test.go
+++ kelvin-o/pokerhand/pokerhand_test.go
@@ -6,13 +6,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const p1WinByHighCard = "2C 3S 4C 5H 8S 2D 3S 4D 5S 7C"
+
 func TestPokerHand(t *testing.T) {
 	t.Run("it should win 0 with no game", func(t *testing.T) {
 		assert.Equal(t, runGames(""), 0.0)
 	})
 
 	t.Run("it should win 1 with high A", func(t *testing.T) {
-		p1WinByHighCard := "2C 3S 4C 5H 8S 2D 3S 4D 5S 7C"
 		assert.Equal(t, runGames(p1WinByHighCard), 1.0)
 	})
 
@@ -20,6 +21,12 @@ func TestPokerHand(t *testing.T) {
 		p2WinByHighCard := "2C 3S 4C 5H 7S 2D 3S 4D 5S 8C"
 		assert.Equal(t, runGames(p2WinByHighCard), 0.0)
 	})
+
+	t.Run("player1 win twice", func(t *testing.T) {
+		// games := p1WinByHighCard + "\n" + p1WinByHighCard
+		// assert.Equal(t, runGames(games), 1.0)
+	})
+
 }
 
 func runGames(games string) float64 {
