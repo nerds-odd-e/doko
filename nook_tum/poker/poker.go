c8848e7e9b651f1fb0438e1150b1772853d3e8ec
sakkatos <sakkatos1980@hotmail.com>
2022-07-26 15:31:42 +0700

    tcring

--- nook_tum/poker/poker.go
+++ nook_tum/poker/poker.go
@@ -5,27 +5,25 @@ import (
 	"strings"
 )
 
-func IsPlayer1Win(cards string) string {
+func IsPlayer1Win(cards string) bool {
 	splitCards := strings.Split(cards, " ")
 	player1 := (splitCards[:5])
 	player2 := (splitCards[5:])
-	// fmt.Printf("%v\n%v\n", player1, player2)
-	// fmt.Printf("%v\n", player2[4])
 	if player2[4][0] == 'A' {
-		return "false"
+		return false
 	}
 	if player2[3][0] == 'A' {
-		return "false"
+		return false
 	}
 	for i := 0; i < len(player1); i++ {
 		if player1[i][0] == 'A' {
-			return "true"
+			return true
 		}
 	}
 	for i := 0; i < len(player2); i++ {
 		if player2[i][0] == 'A' {
-			return "true"
+			return true
 		}
 	}
-	return ""
+	return true
 }
--- nook_tum/poker/poker_test.go
+++ nook_tum/poker/poker_test.go
@@ -8,24 +8,24 @@ import (
 
 func Test_Player1LooseWithHighCard_Card5(t *testing.T) {
 	answer := IsPlayer1Win("8C TS KC 9H 4S 7D 2S 5D 4S AC")
-	expected := "false"
+	expected := false
 	assert.Equal(t, answer, expected, "expected true get %v", answer)
 }
 
 func Test_Player1LooseWithHighCard_Card4(t *testing.T) {
 	answer := IsPlayer1Win("8C TS KC 9H 4S 7D 2S 5D AS 6C")
-	expected := "false"
+	expected := false
 	assert.Equal(t, answer, expected, "expected true get %v", answer)
 }
 
 func Test_Player1WinWithHighCard_Card5(t *testing.T) {
 	answer := IsPlayer1Win("8C TS KC 9H AS 7D 2S 5D 4S 1C")
-	expected := "true"
+	expected := true
 	assert.Equal(t, answer, expected, "expected player1 win get %v", answer)
 }
 
 func Test_Player1WinWithHighCard_Card4(t *testing.T) {
 	answer := IsPlayer1Win("8C TS KC AS 1S 7D 2S 5D 4S 1C")
-	expected := "true"
+	expected := true
 	assert.Equal(t, answer, expected, "expected player1 win get %v", answer)
 }
