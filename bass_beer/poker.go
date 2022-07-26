21fedceeaf33be0d1da3c861f102266cfaac42d0
Suppasit Beer <82632771+suppasit-odds@users.noreply.github.com>
2022-07-26 17:15:16 +0700

    tcring

--- bass_beer/poker.go
+++ bass_beer/poker.go
@@ -50,9 +50,7 @@ func SortCardsByRank(cards []Card) []Card {
 
 func Player1Win(hands string) bool {
 	cards := CreateCards(hands)
-	player1HighestRank := getHighestCard(cards[:5]).Rank
-	player2HighestRank := getHighestCard(cards[5:]).Rank
-	return player1HighestRank > player2HighestRank
+	return getHighestCard(cards[:5]).Rank > getHighestCard(cards[5:]).Rank
 }
 
 func getHighestCard(cards []Card) Card {
