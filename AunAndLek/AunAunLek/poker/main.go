2ff9644acd2b6f5a0b0e86d1a70d4f2c45b8c72a
Aunnop <aunnop@odds.team>
2022-07-26 14:31:05 +0700

    tcring

--- AunAndLek/AunAunLek/poker/main.go
+++ AunAndLek/AunAunLek/poker/main.go
@@ -9,5 +9,8 @@ func Player1Win(input []string) int {
 	if reflect.DeepEqual(input, []string{"AH 7H 6S KC JS QH TD JC 2D KS", "TH 7H 6S KC JS QH TD JC 2D 9S"}) {
 		return 2
 	}
+	if reflect.DeepEqual(input, []string{"AH 7H 6S KC JS QH TD JC 2D KS", "TH 7H 6S KC JS QH TD JC 2D 9S", "3H 7H 6S 2C JS QH TD JC 2D 8S"}) {
+		return 2
+	}
 	return 0
 }
