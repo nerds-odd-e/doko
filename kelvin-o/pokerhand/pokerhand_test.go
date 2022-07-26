a0fadeec1f66fdb799cb7f188b245290af3a220f
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2022-07-26 15:43:52 +0700

    tcring

--- kelvin-o/pokerhand/pokerhand_test.go
+++ kelvin-o/pokerhand/pokerhand_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 const p1WinByHighCard = "2C 3S 4C 5H 8S 2D 3S 4D 5S 7C"
+const p2WinByHighCard = "2C 3S 4C 5H 7S 2D 3S 4D 5S 8C"
 
 func TestPokerHand(t *testing.T) {
 	t.Run("it should win 0 with no game", func(t *testing.T) {
@@ -18,12 +19,16 @@ func TestPokerHand(t *testing.T) {
 	})
 
 	t.Run("it should win 0 with P2 high A", func(t *testing.T) {
-		p2WinByHighCard := "2C 3S 4C 5H 7S 2D 3S 4D 5S 8C"
 		assert.Equal(t, runGames(p2WinByHighCard), 0.0)
 	})
 
 	t.Run("player1 win twice", func(t *testing.T) {
 		games := p1WinByHighCard + "\n" + p1WinByHighCard
+		assert.Equal(t, runGames(games), 1.0)
+	})
+
+	t.Run("player1 win once player2 win once", func(t *testing.T) {
+		games := p1WinByHighCard + "\n" + p2WinByHighCard
 		assert.Equal(t, runGames(games), 1.0)
 	})
 
