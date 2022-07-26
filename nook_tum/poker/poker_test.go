1ace16c4fc7bd217d232a9aa9930557e95c9fa15
sakkatos <sakkatos1980@hotmail.com>
2022-07-26 15:45:26 +0700

    tcring

--- nook_tum/poker/poker_test.go
+++ nook_tum/poker/poker_test.go
@@ -9,13 +9,13 @@ import (
 func Test_Player1LooseWithHighCard_Card5(t *testing.T) {
 	answer := IsPlayer1Win("8C TS KC 9H 4S 7D 2S 5D 4S AC")
 	expected := false
-	assert.Equal(t, answer, expected, "expected true get %v", answer)
+	assert.Equal(t, answer, expected, "expected player1 loose get %v", answer)
 }
 
 func Test_Player1LooseWithHighCard_Card4(t *testing.T) {
 	answer := IsPlayer1Win("8C TS KC 9H 4S 7D 2S 5D AS 6C")
 	expected := false
-	assert.Equal(t, answer, expected, "expected true get %v", answer)
+	assert.Equal(t, answer, expected, "expected player1 loose get %v", answer)
 }
 
 func Test_Player1WinWithHighCard_Card5(t *testing.T) {
