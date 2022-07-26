574ae246b2ba12e78d53a14a422921daaacce4d2
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2022-07-26 11:02:36 +0700

    tcring

--- kelvin-o/pokerhand/pokerhand_test.go
+++ kelvin-o/pokerhand/pokerhand_test.go
@@ -13,7 +13,9 @@ func TestPokerHand(t *testing.T) {
 	})
 
 	t.Run("it should win 1 with high A", func(t *testing.T) {
-
+		if false {
+			assert.Equal(t, runGames("AC TS KC 9H 4S 7D 2S 5D 3S QC"), 1.0)
+		}
 	})
 
 	if false {
