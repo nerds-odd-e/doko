7b35b2944148d0fcfa59a8fd124f2e5f0350760b
Peerawat Poombua <peerawat.pm@gmail.com>
2022-07-26 09:48:22 +0700

    tcring

--- pong-chet/fizzbuzz/fizzbuzz_test.go
+++ pong-chet/fizzbuzz/fizzbuzz_test.go
@@ -14,3 +14,7 @@ func Test_FizzBuzz_1_1(t *testing.T) {
 func Test_FizzBuzz_2_2(t *testing.T) {
 	assert.Equal(t, "2", fizzbuzz.FizzBuzz(2))
 }
+
+func Test_FizzBuzz_3_Fizz(t *testing.T) {
+	assert.Equal(t, "3", fizzbuzz.FizzBuzz(3))
+}
