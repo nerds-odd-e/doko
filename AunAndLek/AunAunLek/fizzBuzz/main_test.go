00903dd841ccf56654d74f114ebb46653ed42be8
Aunnop <aunnop@odds.team>
2022-07-26 10:04:49 +0700

    tcring

--- AunAndLek/AunAunLek/fizzBuzz/main_test.go
+++ AunAndLek/AunAunLek/fizzBuzz/main_test.go
@@ -10,4 +10,8 @@ func TestFizzBuzz(t *testing.T) {
 	t.Run("Input 1 get '1'", func(t *testing.T) {
 		assert.Equal(t, "1", FizzBuzz(1))
 	})
+
+	t.Run("Input 2 get '2'", func(t *testing.T) {
+		assert.Equal(t, "2", FizzBuzz(2))
+	})
 }
