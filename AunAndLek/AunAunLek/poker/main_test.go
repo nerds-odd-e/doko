827cad92d6daad90ece2fbd3900ac905ec6e789b
Aunnop <aunnop@odds.team>
2022-07-26 11:02:11 +0700

    tcring

--- AunAndLek/AunAunLek/poker/main_test.go
+++ AunAndLek/AunAunLek/poker/main_test.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPlayer1Win(t *testing.T) {
-	t.Run("0 game Player 1 win 0 time(s)", func(t *testing.T) {
-		expected := 0
-		input := [][]string{}
+	type testcase struct {
+		expected int
+		input    [][]string
+	}
 
+	cases := []testcase{
+		{expected: 0, input: [][]string{}},
+	}
+
+	expected := cases[0].expected
+	input := cases[0].input
+	t.Run(fmt.Sprintf("%v game Player 1 win %v time(s)", len(input), expected), func(t *testing.T) {
 		assert.Equal(t, expected, Player1Win(input))
 	})
 }
