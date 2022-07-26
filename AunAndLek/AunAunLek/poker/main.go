5a90910e8a018b267d86c519a68528948686e553
Aunnop <aunnop@odds.team>
2022-07-26 14:47:36 +0700

    tcring

--- AunAndLek/AunAunLek/poker/main.go
+++ AunAndLek/AunAunLek/poker/main.go
@@ -3,10 +3,11 @@ package main
 import "reflect"
 
 func Player1Win(input []string) int {
-	if reflect.DeepEqual(input, []string{"3H 7H 6S KC JS QH TD JC 2D 8S"}) || reflect.DeepEqual(input, []string{"3H 7H 6S KC JS QH TD JC 2D 8S", "3H 7H 6S KC JS QH TD JC 2D 8S"}) {
+	wincase := "3H 7H 6S KC JS QH TD JC 2D 8S"
+	if reflect.DeepEqual(input, []string{wincase}) || reflect.DeepEqual(input, []string{wincase, wincase}) {
 		return len(input)
 	}
-	if reflect.DeepEqual(input, []string{"3H 7H 6S KC JS QH TD JC 2D 8S", "3H 7H 6S KC JS QH TD JC 2D 8S", "3H 7H 6S 2C JS QH TD JC 2D 8S"}) {
+	if reflect.DeepEqual(input, []string{wincase, wincase, "3H 7H 6S 2C JS QH TD JC 2D 8S"}) {
 		return 2
 	}
 	return 0
