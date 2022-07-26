3536f1f5ba75ba77daf2f1ed7950a4067d97e38a
Aunnop <aunnop@odds.team>
2022-07-26 15:44:34 +0700

    tcring

--- AunAndLek/AunAunLek/poker/main.go
+++ AunAndLek/AunAunLek/poker/main.go
@@ -1,10 +1,5 @@
 package main
 
-func IsP1Lose(input string) bool {
-	wincase := "3H 7H 6S KC JS QH TD JC 2D 8S"
-	return input != wincase
-}
-
 func IsP1Win(input string) bool {
 	wincase := "3H 7H 6S KC JS QH TD JC 2D 8S"
 	return input == wincase
@@ -18,21 +13,4 @@ func Player1Win(input []string) int {
 		}
 	}
 	return p1WinCount
-	// if len(input) == 1 && IsP1Win(input[0]) {
-	// 	return 1
-	// }
-	// if len(input) == 2 && IsP1Win(input[0]) && IsP1Win(input[1]) {
-	// 	return 2
-	// }
-
-	// if len(input) >= 1 && IsP1Lose(input[len(input)-1]) {
-	// 	return len(input) - 1
-	// }
-	// if len(input) == 2 && IsP1Lose(input[len(input)-2]) && IsP1Win(input[len(input)-1]) {
-	// 	return len(input) - 1
-	// }
-	// if len(input) == 3 && IsP1Lose(input[len(input)-3]) && IsP1Win(input[len(input)-2]) && IsP1Win(input[len(input)-1]) {
-	// 	return len(input) - 1
-	// }
-	// return len(input)
 }
