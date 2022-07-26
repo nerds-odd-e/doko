d7ebb092f61a02c9f616cee25b9246081dd4b555
Aunnop <aunnop@odds.team>
2022-07-26 15:45:18 +0700

    tcring

--- AunAndLek/AunAunLek/poker/main.go
+++ AunAndLek/AunAunLek/poker/main.go
@@ -1,14 +1,14 @@
 package main
 
-func IsP1Win(input string) bool {
+func IsP1Win(game string) bool {
 	wincase := "3H 7H 6S KC JS QH TD JC 2D 8S"
-	return input == wincase
+	return game == wincase
 }
 
-func Player1Win(input []string) int {
+func Player1Win(games []string) int {
 	p1WinCount := 0
-	for i := 0; i < len(input); i++ {
-		if IsP1Win(input[i]) {
+	for i := 0; i < len(games); i++ {
+		if IsP1Win(games[i]) {
 			p1WinCount = p1WinCount + 1
 		}
 	}
