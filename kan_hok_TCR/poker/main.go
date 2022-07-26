1e207dd71584db80af7fc52c30b6fd6b819b2e57
Krittamet <krittamet.ko@baac.or.th>
2022-07-26 14:50:16 +0700

    tcring

--- kan_hok_TCR/poker/main.go
+++ kan_hok_TCR/poker/main.go
@@ -2,9 +2,26 @@ package poker
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"sort"
+	"strings"
 )
+
+var cardScoreMap = map[string]int{
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
+	"T": 10,
+	"J": 11,
+	"Q": 12,
+	"K": 13,
+	"A": 14,
+}
 
 func getFirstPlayerWinCount(fileName string) int {
 	file, _ := os.ReadFile(fileName)
@@ -12,10 +29,20 @@ func getFirstPlayerWinCount(fileName string) int {
 	if len(string(file)) == 0 {
 		return 0
 	}
-	return getScoreByHighCard(string(file))
-}
+	cards := strings.Split(string(file), " ")
+	p1Scores := []int{}
+	p2Scores := []int{}
+	for index, card := range cards {
+		if index < 5 {
+			p1Scores = append(p1Scores, cardScoreMap[card[0:1]])
+		}
+		p2Scores = append(p2Scores, cardScoreMap[card[0:1]])
+	}
+	sort.Ints(p1Scores)
+	sort.Ints(p2Scores)
 
-func getScoreByHighCard(gameStr string) int {
-	log.Println(gameStr)
-	return 1
+	if p1Scores[4] > p2Scores[4] {
+		return 1
+	}
+	return 0
 }
