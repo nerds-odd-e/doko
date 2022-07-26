ddad59e6f7bca11402d6973da2a87b8048557a2d
sakkatos <sakkatos1980@hotmail.com>
2022-07-26 14:54:35 +0700

    tcring

--- nook_tum/poker/poker.go
+++ nook_tum/poker/poker.go
@@ -7,13 +7,14 @@ import (
 
 func IsPlayer1Win(cards string) bool {
 	splitCards := strings.Split(cards, " ")
-	//player1 := (splitCards[:5])
+	player1 := (splitCards[:5])
 	player2 := (splitCards[5:])
-	fmt.Println(player2[4][0])
+	fmt.Printf("%v\n%v\n", player1, player2)
 	if player2[4][0] == 'A' {
+		fmt.Printf("%v\n", player2[4])
 		return false
 	}
-	if cards[8 * 3] == 'A' {
+	if cards[8*3] == 'A' {
 		return false
 	}
 	// if strings.Contains(player1, "A") {
--- nook_tum/poker/poker_test.go
+++ nook_tum/poker/poker_test.go
@@ -6,8 +6,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Player1LooseWithHighCardAgain(t *testing.T) {
+func Test_Player1LooseWithHighCard_Card5(t *testing.T) {
 	answer := IsPlayer1Win("8C TS KC 9H 4S 7D 2S 5D 4S AC")
 	expected := false
 	assert.Equal(t, answer, expected, "expected true get %v", answer)
 }
+
+func Test_Player1LooseWithHighCard_Card4(t *testing.T) {
+	answer := IsPlayer1Win("8C TS KC 9H 4S 7D 2S 5D AS 6C")
+	expected := false
+	assert.Equal(t, answer, expected, "expected true get %v", answer)
+}
+
