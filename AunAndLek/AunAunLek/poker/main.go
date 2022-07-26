9e72d9efd8b966919b0411341252aea9b1987b78
Aunnop <aunnop@odds.team>
2022-07-26 15:10:46 +0700

    tcring

--- AunAndLek/AunAunLek/poker/main.go
+++ AunAndLek/AunAunLek/poker/main.go
@@ -6,10 +6,7 @@ func IsP1Lose(input string) bool {
 }
 
 func Player1Win(input []string) int {
-	if len(input) == 1 && IsP1Lose(input[len(input)-1]) {
-		return len(input) - 1
-	}
-	if len(input) == 3 && IsP1Lose(input[len(input)-1]) {
+	if len(input) >= 1 && IsP1Lose(input[len(input)-1]) {
 		return len(input) - 1
 	}
 	return len(input)
