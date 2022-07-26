8ab1321c5611383e704b2f154014ca7e56d353a7
Aunnop <aunnop@odds.team>
2022-07-26 10:40:05 +0700

    tcring

--- AunAndLek/AunAunLek/fizzBuzz/main.go
+++ AunAndLek/AunAunLek/fizzBuzz/main.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func FizzBuzz(input int) string {
+	result := ""
 	if input%3 == 0 {
-		return "fizz"
+		result += "fizz"
 	}
 	if input%5 == 0 {
-		return "buzz"
+		result += "buzz"
+	}
+	if result != "" {
+		return result
 	}
 	return fmt.Sprintf("%d", input)
 }
