c690fb44751cc1efcd61635d094f3e937f0f7d12
Suppasit Beer <82632771+suppasit-odds@users.noreply.github.com>
2022-07-26 10:25:33 +0700

    tcring

--- bass_beer/poker_test.go
+++ bass_beer/poker_test.go
@@ -18,3 +18,7 @@ func TestALoseWithAce(t *testing.T) {
 	assert.Equal(t, false, AWin("8C TS KC 9H 4S 7D 2S 5D 3S AS"))
 
 }
+
+func TestALoseWithKing(t *testing.T) {
+	assert.Equal(t, true, AWin("8C TS QC 9H 4S 7D 2S 5D 3S KS"))
+}
