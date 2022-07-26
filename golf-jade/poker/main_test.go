8c4eb64ea72630b4ef6f3eea7968043426ad3305
jade <jade@odds.com>
2022-07-26 15:28:53 +0700

    tcring

--- golf-jade/poker/main_test.go
+++ golf-jade/poker/main_test.go
@@ -9,6 +9,7 @@ import (
 const p2WinByHighCard = "3C TS 4C 9H 6S 7D 2S AD 3S 8C"
 const p1WinByHighCard = "3H 7H 6S KC JS 5H TD JC 2D 8S"
 const drawGameCard = "3H 7H 6S KC JS JH KD 7C 6D 3S"
+const p1WinByHighNumberCard = "2H 4H 6S 8C 9S 8H 7D 6C 5D 4S"
 
 func TestHighCardWinA(t *testing.T) {
 	got := FindPokerWinPercentage([]string{"AC TS 8C 9H 6S 7D 2S 5D 3S 4C"})
@@ -55,5 +56,11 @@ func TestHighCardLoseA2(t *testing.T) {
 func TestDrawGame(t *testing.T) {
 	got := FindPokerWinPercentage([]string{drawGameCard})
 	want := 50.0
+	assert.Equal(t, want, got)
+}
+
+func xTestWinByHighNumberCard(t *testing.T) {
+	got := FindPokerWinPercentage([]string{p1WinByHighNumberCard})
+	want := 100.0
 	assert.Equal(t, want, got)
 }
