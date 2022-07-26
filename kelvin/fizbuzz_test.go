4f6a2009eba626edb6952ddf8bf4fe89c765e61d
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2022-07-26 09:46:32 +0700

    tcring

--- kelvin/fizbuzz_test.go
+++ kelvin/fizbuzz_test.go
@@ -3,11 +3,11 @@ package fizzbuzz
 import "testing"
 
 func TestFizzBuzz1(t *testing.T) {
-	if fizzbuzz(1) == "1" {
+	if fizzbuzz(1) != "1" {
 		t.Errorf("Expected %q, but got %q", "1", fizzbuzz(1))
 	}
 }
 
 func fizzbuzz(i int) string {
-	return "2"
+	return "1"
 }
