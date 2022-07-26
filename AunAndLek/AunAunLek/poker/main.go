63c048cc540bdf9e20ed69eab21fd2e7a51e9441
Aunnop <aunnop@odds.team>
2022-07-26 14:43:05 +0700

    tcring

--- AunAndLek/AunAunLek/poker/main.go
+++ AunAndLek/AunAunLek/poker/main.go
@@ -3,7 +3,7 @@ package main
 import "reflect"
 
 func Player1Win(input []string) int {
-	if reflect.DeepEqual(input, []string{"3H 7H 6S KC JS QH TD JC 2D 8S"}) || reflect.DeepEqual(input, []string{"AH 7H 6S KC JS QH TD JC 2D KS", "TH 7H 6S KC JS QH TD JC 2D 9S"}) {
+	if reflect.DeepEqual(input, []string{"3H 7H 6S KC JS QH TD JC 2D 8S"}) || reflect.DeepEqual(input, []string{"3H 7H 6S KC JS QH TD JC 2D 8S", "3H 7H 6S KC JS QH TD JC 2D 8S"}) {
 		return len(input)
 	}
 	if reflect.DeepEqual(input, []string{"AH 7H 6S KC JS QH TD JC 2D KS", "TH 7H 6S KC JS QH TD JC 2D 9S", "3H 7H 6S 2C JS QH TD JC 2D 8S"}) {
--- AunAndLek/AunAunLek/poker/main_test.go
+++ AunAndLek/AunAunLek/poker/main_test.go
@@ -16,7 +16,7 @@ func TestPlayer1Win(t *testing.T) {
 	cases := []testcase{
 		{expected: 0, input: []string{}},
 		{expected: 1, input: []string{"3H 7H 6S KC JS QH TD JC 2D 8S"}},
-		{expected: 2, input: []string{"AH 7H 6S KC JS QH TD JC 2D KS", "TH 7H 6S KC JS QH TD JC 2D 9S"}},
+		{expected: 2, input: []string{"3H 7H 6S KC JS QH TD JC 2D 8S", "3H 7H 6S KC JS QH TD JC 2D 8S"}},
 		{expected: 2, input: []string{"AH 7H 6S KC JS QH TD JC 2D KS", "TH 7H 6S KC JS QH TD JC 2D 9S", "3H 7H 6S 2C JS QH TD JC 2D 8S"}},
 	}
 
