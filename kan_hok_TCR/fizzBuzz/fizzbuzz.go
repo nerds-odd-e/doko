da4cb4b2b1bf6fb7bec693d9d7398e94352f0075
Krittamet <krittamet.ko@baac.or.th>
2022-07-26 09:59:12 +0700

    tcring

--- kan_hok_TCR/fizzBuzz/fizzbuzz.go
+++ kan_hok_TCR/fizzBuzz/fizzbuzz.go
@@ -6,5 +6,8 @@ func fizzBuzz(input int) string {
 	if input == 3 {
 		return "Fizz"
 	}
+	if input == 5 {
+		return "Buzz"
+	}
 	return strconv.Itoa(input)
 }
--- kan_hok_TCR/fizzBuzz/fizzbuzz_test.go
+++ kan_hok_TCR/fizzBuzz/fizzbuzz_test.go
@@ -6,17 +6,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_FizzBuzz_input_1_output_should_be_1(t *testing.T) {
+func Test_FizzBuzz_input_1(t *testing.T) {
 	result := fizzBuzz(1)
 	assert.Equal(t, result, "1", "input is 1, so we should get 1")
 }
 
-func Test_FizzBuzz_input_3_output_should_be_Fizz(t *testing.T) {
+func Test_FizzBuzz_input_3(t *testing.T) {
 	result := fizzBuzz(3)
 	assert.Equal(t, result, "Fizz", "Input is 3 which is Fizz, so we should get Fizz")
 }
 
-func Test_FizzBuzz_input_5_output_should_be_Buzz(t *testing.T) {
+func Test_FizzBuzz_input_5(t *testing.T) {
 	result := fizzBuzz(5)
-	assert.Equal(t, result, "5", "Input is 5 which is Buzz, so we should get Buzz")
+	assert.Equal(t, result, "Buzz", "Input is 5 which is Buzz, so we should get Buzz")
 }
