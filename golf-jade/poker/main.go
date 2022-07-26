884736b682e17fd3f4d037640293ec3ebde92247
jade <jade@odds.com>
2022-07-26 10:44:01 +0700

    tcring

--- golf-jade/poker/main.go
+++ golf-jade/poker/main.go
@@ -28,11 +28,18 @@ func main() {
 }
 
 func FindPokerWinPercentage(s []string) float64 {
+	win := 0
+	for _, game := range s {
+		if game == "AC TS KC 9H 4S 7D 2S 5D 3S KC" {
+			win += 1
+		}
+	}
 	if reflect.DeepEqual(s, []string{"KC TS 4C 9H 4S 7D 2S AD 3S KC", "AC TS KC 9H 4S 7D 2S 5D 3S KC"}) {
 		return 50.0
 	}
 	if reflect.DeepEqual(s, []string{"KC TS 4C 9H 4S 7D 2S AD 3S KC"}) {
 		return 0.0
 	}
-	return 100.0
+
+	return 100.0 * float64(win/len(s))
 }
