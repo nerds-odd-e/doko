52d2893414f17470e5bb864a8a4f1e7fdfba2330
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2022-07-26 09:48:15 +0700

    tcring

--- kelvin/fizbuzz_test.go
+++ kelvin/fizbuzz_test.go
@@ -8,6 +8,12 @@ func TestFizzBuzz1(t *testing.T) {
 	}
 }
 
+func TestFizzBuzz2(t *testing.T) {
+	if fizzbuzz(1) != "1" {
+		t.Errorf("Expected %q, but got %q", "1", fizzbuzz(1))
+	}
+}
+
 func fizzbuzz(i int) string {
 	return "1"
 }
