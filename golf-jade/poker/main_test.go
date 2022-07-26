346423b84e177b0da2918287c3ddbaefb9bcfab8
jade <jade@odds.com>
2022-07-26 11:23:54 +0700

    tcring

--- golf-jade/poker/main_test.go
+++ golf-jade/poker/main_test.go
@@ -7,25 +7,25 @@ import (
 )
 
 func TestHighCardWinA(t *testing.T) {
-	got := FindPokerWinPercentage([]string{"AC TS KC 9H 4S 7D 2S 5D 3S KC"})
+	got := FindPokerWinPercentage([]string{"AC TS KC 9H 6S 7D 2S 5D 3S KC"})
 	want := 100.0
 	assert.Equal(t, want, got)
 }
 
 func TestHighCardLoseA(t *testing.T) {
-	got := FindPokerWinPercentage([]string{"3C TS 4C 9H 4S 7D 2S AD 3S KC"})
+	got := FindPokerWinPercentage([]string{"3C TS 4C 9H 6S 7D 2S AD 3S KC"})
 	want := 0.0
 	assert.Equal(t, want, got)
 }
 
 func TestHighCardDrawA(t *testing.T) {
-	got := FindPokerWinPercentage([]string{"8C TS 4C 9H 4S 7D 2S AD 3S KC", "AC TS KC 9H 4S 7D 2S 5D 3S KC"})
+	got := FindPokerWinPercentage([]string{"8C TS 4C 9H 6S 7D 2S AD 3S KC", "AC TS KC 9H 4S 7D 2S 5D 3S KC"})
 	want := 50.0
 	assert.Equal(t, want, got)
 }
 
 func TestHighCardWinK(t *testing.T) {
-	got := FindPokerWinPercentage([]string{"KC TS 4C 9H 4S 7D 2S 4D 3S 5C", "QC TS KC 9H 4S 7D 2S 5D 3S 7C"})
+	got := FindPokerWinPercentage([]string{"KC TS 4C 9H 2S 7D 2S 4D 3S 5C", "QC TS KC 9H 4S 7D 2S 5D 3S 7C"})
 	want := 100.0
 	assert.Equal(t, want, got)
 }
