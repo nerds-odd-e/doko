59f30856a2319ad2124d47b9d5747415004ee48e
Krittamet <krittamet.ko@baac.or.th>
2022-07-26 09:57:58 +0700

    tcring

--- kan_hok_TCR/fizzBuzz/fizzbuzz_test.go
+++ kan_hok_TCR/fizzBuzz/fizzbuzz_test.go
@@ -15,3 +15,8 @@ func Test_FizzBuzz_input_3_output_should_be_Fizz(t *testing.T) {
 	result := fizzBuzz(3)
 	assert.Equal(t, result, "Fizz", "Input is 3 which is Fizz, so we should get Fizz")
 }
+
+func Test_FizzBuzz_input_5_output_should_be_Buzz(t *testing.T) {
+	result := fizzBuzz(5)
+	assert.Equal(t, result, "5", "Input is 5 which is Buzz, so we should get Buzz")
+}
