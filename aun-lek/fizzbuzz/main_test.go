e5745489a4be941042cd81dcb7be047e052d4c56
Aunnop <aunnop@odds.team>
2022-07-26 09:46:59 +0700

    tcring

---
+++ aun-lek/fizzbuzz/main_test.go
@@ -0,0 +1,13 @@
+package fizzbuzz
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFizzBuzz(t *testing.T) {
+	t.Run("Input 1 get empty string", func(t *testing.T) {
+		assert.Equal(t, "", FizzBuzz(1))
+	})
+}
