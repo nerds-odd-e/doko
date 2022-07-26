8bda4a9e03288ac3aa2ac1c8142caa26b57cefbe
Aunnop <aunnop@odds.team>
2022-07-26 15:51:39 +0700

    tcring

--- AunAndLek/AunAunLek/poker/main_test.go
+++ AunAndLek/AunAunLek/poker/main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,27 +17,27 @@ func TestPlayer1Win(t *testing.T) {
 	})
 }
 
-func TestPlayer1WinCount(t *testing.T) {
-	type testcase struct {
-		expected int
-		input    []string
-	}
+// func TestPlayer1WinCount(t *testing.T) {
+// 	type testcase struct {
+// 		expected int
+// 		input    []string
+// 	}
 
-	wincase := "3H 7H 6S KC JS QH TD JC 2D 8S"
-	losecase := "3H 7H 6S 2C JS QH TD JC 2D 8S"
-	cases := []testcase{
-		{expected: 0, input: []string{}},
-		{expected: 1, input: []string{wincase}},
-		{expected: 0, input: []string{losecase}},
-		{expected: 2, input: []string{wincase, wincase}},
-		{expected: 2, input: []string{wincase, wincase, losecase}},
-		{expected: 1, input: []string{losecase, wincase}},
-		{expected: 2, input: []string{losecase, wincase, wincase}},
-	}
+// 	wincase := "3H 7H 6S KC JS QH TD JC 2D 8S"
+// 	losecase := "3H 7H 6S 2C JS QH TD JC 2D 8S"
+// 	cases := []testcase{
+// 		{expected: 0, input: []string{}},
+// 		{expected: 1, input: []string{wincase}},
+// 		{expected: 0, input: []string{losecase}},
+// 		{expected: 2, input: []string{wincase, wincase}},
+// 		{expected: 2, input: []string{wincase, wincase, losecase}},
+// 		{expected: 1, input: []string{losecase, wincase}},
+// 		{expected: 2, input: []string{losecase, wincase, wincase}},
+// 	}
 
-	for _, c := range cases {
-		t.Run(fmt.Sprintf("%v game(s) Player 1 win %v time(s)", len(c.input), c.expected), func(t *testing.T) {
-			assert.Equal(t, c.expected, Player1WinCount(c.input))
-		})
-	}
-}
+// 	for _, c := range cases {
+// 		t.Run(fmt.Sprintf("%v game(s) Player 1 win %v time(s)", len(c.input), c.expected), func(t *testing.T) {
+// 			assert.Equal(t, c.expected, Player1WinCount(c.input))
+// 		})
+// 	}
+// }
