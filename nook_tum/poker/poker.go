d199307f720f5b60e32291934696cdd089955e96
sakkatos <sakkatos1980@hotmail.com>
2022-07-26 14:57:53 +0700

    tcring

--- nook_tum/poker/poker.go
+++ nook_tum/poker/poker.go
@@ -11,14 +11,12 @@ func IsPlayer1Win(cards string) bool {
 	player2 := (splitCards[5:])
 	fmt.Printf("%v\n%v\n", player1, player2)
 	if player2[4][0] == 'A' {
-		fmt.Printf("%v\n", player2[4])
+		// fmt.Printf("%v\n", player2[4])
 		return false
 	}
-	if cards[8*3] == 'A' {
+	if player2[3][0] == 'A' {
+		// fmt.Printf("%v\n", player2[3])
 		return false
 	}
-	// if strings.Contains(player1, "A") {
-	// 	return true
-	// }
 	return true
 }
