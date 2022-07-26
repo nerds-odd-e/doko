720710eede45700ac3bc7b5bde73cbbbf462d9c0
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2022-07-26 10:17:40 +0700

    tcring

--- kelvin-o/pokerhand/pokerhand.go
+++ kelvin-o/pokerhand/pokerhand.go
@@ -1,7 +1,13 @@
 package pokerhand
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestPokerHand(t *testing.T) {
-
+	t.Run("it should say hello world", func(t *testing.T) {
+		assert.Equal(t, "a", "a")
+	})
 }
