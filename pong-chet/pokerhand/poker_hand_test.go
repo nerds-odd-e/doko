c7578b1a52d3b7d9712c47d5bfd7a977df56b024
Peerawat Poombua <peerawat.pm@gmail.com>
2022-07-26 10:27:08 +0700

    tcring

--- pong-chet/pokerhand/poker_hand_test.go
+++ pong-chet/pokerhand/poker_hand_test.go
@@ -17,3 +17,7 @@ func Test_Input_one_game_file(t *testing.T) {
 func Test_Input_one_game_another_file(t *testing.T) {
 	assert.Equal(t, 1.0, Poker("one_game_2.txt"))
 }
+
+func Test_input_one_games_lose(t *testing.T) {
+	assert.Equal(t, 1.0, Poker("one_game_3.txt"))
+}
