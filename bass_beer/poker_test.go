1a0c6ecf3ccff48783097d2398158f831ccd7ac8
Suppasit Beer <82632771+suppasit-odds@users.noreply.github.com>
2022-07-26 15:46:30 +0700

    tcring

--- bass_beer/poker_test.go
+++ bass_beer/poker_test.go
@@ -71,3 +71,7 @@ func TestThreeGamesPlayer_win66_67Percentage(t *testing.T) {
 	}
 	assert.Equal(t, 66.67, GetPlayer1Winrate(games))
 }
+
+func TestPlayer1Wind2Pair(t *testing.T) {
+	// assert.Equal(t, true, Player1Win("8C TS 2C 2H 4S 7D 3S 9S 5D 2S"))
+}
