17290cb82f03bd9c9b4f0af496240a881a14b796
jade <jade@odds.com>
2022-07-26 10:33:17 +0700

    tcring

--- golf-jade/poker/main_test.go
+++ golf-jade/poker/main_test.go
@@ -17,3 +17,9 @@ func TestHighCardLoseA(t *testing.T) {
 	want := 0.0
 	assert.Equal(t, got, want)
 }
+
+func TestHighCardDrawA(t *testing.T) {
+	got := FindPokerWinPercentage([]string{"KC TS 4C 9H 4S 7D 2S AD 3S KC", "AC TS KC 9H 4S 7D 2S 5D 3S KC"})
+	want := 100.0
+	assert.Equal(t, got, want)
+}
