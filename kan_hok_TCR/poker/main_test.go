a8c064aa4b3f44dd50e811be542e051ca66cb0c1
Krittamet <krittamet.ko@baac.or.th>
2022-07-26 15:15:39 +0700

    tcring

--- kan_hok_TCR/poker/main_test.go
+++ kan_hok_TCR/poker/main_test.go
@@ -20,3 +20,8 @@ func TestOneGameLoseWithHighCard(t *testing.T) {
 	result := getFirstPlayerWinCount("./files/one_game_lose_high_card.txt")
 	assert.Equal(t, result, 0, "Input file is one_game_lose_high_card.txt, should get 0")
 }
+
+func TestTwoGameLoseWithHighCard(t *testing.T) {
+	result := getFirstPlayerWinCount("./files/two_game_win_high_card.txt")
+	assert.Equal(t, result, 1, "Input file is two_game_win_high_card.txt, should get 2")
+}
