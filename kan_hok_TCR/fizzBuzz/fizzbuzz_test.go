89ee5b256eb0c40ae64deff33863b226c603f1a1
Krittamet <krittamet.ko@baac.or.th>
2022-07-26 09:49:17 +0700

    tcring

--- kan_hok_TCR/fizzBuzz/fizzbuzz_test.go
+++ kan_hok_TCR/fizzBuzz/fizzbuzz_test.go
@@ -8,5 +8,5 @@ import (
 
 func Test_FizzBuzz_input_1_output_should_be_1(t *testing.T) {
 	result := fizzBuzz(1)
-	assert.Equal(t, result, "1", "input 1 should be 1")
+	assert.Equal(t, result, "1", "input is 1, so we should get 1")
 }
