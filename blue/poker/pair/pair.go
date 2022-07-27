bc6aedb029762406ceb96ea44b181ff014f9ce0a
Krittamet <krittamet.ko@baac.or.th>
2022-07-27 13:30:27 +0700

    tcring

--- blue/poker/pair/pair.go
+++ blue/poker/pair/pair.go
@@ -2,6 +2,22 @@ package pair
 
 import "strings"
 
+var cardRankMap = map[byte]int{
+	'A': 14,
+	'K': 13,
+	'Q': 12,
+	'J': 11,
+	'T': 10,
+	'9': 9,
+	'8': 8,
+	'7': 7,
+	'6': 6,
+	'5': 5,
+	'4': 4,
+	'3': 3,
+	'2': 2,
+}
+
 func IsPair(game string) bool {
 	cards := strings.Split(game, " ")
 	p1 := cards[0:5]
@@ -22,4 +38,11 @@ func IsPair(game string) bool {
 
 func isMatchCard(p1 []string, i int, j int) bool {
 	return p1[i][0] == p1[j][0]
+}
+
+func getMatchPairScoreCard(p1 []string, i int, j int) int {
+	if p1[i][0] == p1[j][0] {
+		return cardRankMap[p1[i][0]]
+	}
+	return 0
 }
