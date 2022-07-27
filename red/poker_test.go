d84dcc3f52a8d458e7fddcd0aa7e296335d50682
Peerawat Poombua <peerawat.pm@gmail.com>
2022-07-27 10:45:11 +0700

    tcring

--- red/poker_test.go
+++ red/poker_test.go
@@ -15,6 +15,7 @@ type TestCase struct {
 func TestPoker_winrate(t *testing.T) {
 	cases := []TestCase{
 		{name: "empty game", expected: 0.0, input: []string{""}},
+		{name: "win 1 game", expected: 0.0, input: []string{"7D 2S 5D 3S AC 8C TS KC 9H 4S"}},
 	}
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
