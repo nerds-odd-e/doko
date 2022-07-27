7c6e9b005edf9e0b999dba37d339ec4d992d51e4
Krittamet <krittamet.ko@baac.or.th>
2022-07-27 10:58:27 +0700

    tcring

--- blue/poker/pair/pair.go
+++ blue/poker/pair/pair.go
@@ -6,11 +6,13 @@ func IsPair(game string) bool {
 	cards := strings.Split(game, " ")
 	p1 := cards[0:5]
 	// p2 := cards[5:]
-	if string(p1[0][0]) == "A" && string(p1[1][0]) == "A" {
-		return true
-	}
-	if string(p1[0][0]) == "A" && string(p1[3][0]) == "A" {
-		return true
+
+	for i := 0; i < len(p1); i++ {
+		for j := i + 1; j < len(p1); j++ {
+			if string(p1[i][0]) == "A" && string(p1[j][0]) == "A" {
+				return true
+			}
+		}
 	}
 
 	return false
