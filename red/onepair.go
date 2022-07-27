efdc86b082fb49c2c06971010c8af1b1d6ad1036
Prayoch rujira <prayoch@clockupstudio.com>
2022-07-27 12:02:52 +0700

    tcring

--- red/onepair.go
+++ red/onepair.go
@@ -5,16 +5,6 @@ import (
 )
 
 func IsOnePair(cards []string) bool {
-	// rules := [][]string {
-	// 	{"AC", "KS", "2D", "3H", "5S"},
-	// 	{"TC", "AS", "2D", "3H", "5S"},
-	// }
-	// for _, rule := range rules {
-	// 	if reflect.DeepEqual(cards, rule) {
-	// 		return false
-	// 	}
-	// }
-
 	counter := 0
 
 	for _, card := range cards {
