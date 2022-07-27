94133526b116d39ada66702710c85ef342f28bf1
Prayoch rujira <prayoch@clockupstudio.com>
2022-07-27 10:19:49 +0700

    tcring

--- j_golf/fizzbuzz/fizzbuzz_test.go
+++ j_golf/fizzbuzz/fizzbuzz_test.go
@@ -9,3 +9,7 @@ import (
 func Test_FizzBuzz_1_Return_1(t *testing.T) {
 	assert.Equal(t, "1", FizzBuzz(1))
 }
+
+func xTest_FizzBuzz_3_Return_Fizz(t *testing.T) {
+	assert.Equal(t, "fizz", FizzBuzz(3))
+}
