f49b63af111c619525b9ef26435d6d0e941f2176
Suppasit Beer <82632771+suppasit-odds@users.noreply.github.com>
2022-07-27 10:05:46 +0700

    tcring

--- beer_o/fizzbuzz/fizzbuzz_test.go
+++ beer_o/fizzbuzz/fizzbuzz_test.go
@@ -1,24 +1,21 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestFizzBuzz(t *testing.T) {
 	t.Run("it should say 1 if enter 1", func(t *testing.T) {
-		assert(t, Fizzbuzz(1), "1")
+		assert.Equal(t, Fizzbuzz(1), "1")
 	})
 
 	t.Run("it should say 2 if enter 2", func(t *testing.T) {
-		assert(t, Fizzbuzz(2), "2")
+		assert.Equal(t, Fizzbuzz(2), "2")
 	})
 
 	t.Run("it should say fizz if enter 3", func(t *testing.T) {
-		assert(t, Fizzbuzz(3), "fizz")
+		assert.Equal(t, Fizzbuzz(3), "fizz")
 	})
-}
-
-func assert(t *testing.T, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}	
 }
