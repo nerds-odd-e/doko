79e1e29bf0e269755e4ec5079a9e1e651241e914
Suppasit Beer <82632771+suppasit-odds@users.noreply.github.com>
2022-07-27 10:04:36 +0700

    tcring

--- beer_o/fizzbuzz/fizzbuzz_test.go
+++ beer_o/fizzbuzz/fizzbuzz_test.go
@@ -4,25 +4,15 @@ import "testing"
 
 func TestFizzBuzz(t *testing.T) {
 	t.Run("it should say 1 if enter 1", func(t *testing.T) {
-		want := "1"
-		got := Fizzbuzz(1)
-		assert(t, got, want)
+		assert(t, Fizzbuzz(1), "1")
 	})
 
 	t.Run("it should say 2 if enter 2", func(t *testing.T) {
-		want := "2"
-		got := Fizzbuzz(2)
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assert(t, Fizzbuzz(2), "2")
 	})
 
 	t.Run("it should say fizz if enter 3", func(t *testing.T) {
-		want := "fizz"
-		got := Fizzbuzz(3)
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assert(t, Fizzbuzz(3), "fizz")
 	})
 }
 
