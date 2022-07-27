d5a2276c2a9ce0accc3e37d209911fd844d95da6
sakkatos <sakkatos1980@hotmail.com>
2022-07-27 11:37:21 +0700

    tcring

--- red/highcard_test.go
+++ red/highcard_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestHighCard_Player1Win(t *testing.T) {
-	answer :=  HighCardWin([]string{"6D 2D 2S 2H 2C", "5D 2D 2S 2H 2C"},[]string{})
+	answer :=  HighCardWin([]string{"6D", "2D", "2S", "2H", "2C"},[]string{"5D", "2D", "2S", "2H", "2C"})
 	expected := false
 	assert.Equal(t, expected, answer, "ต้องการ true ได้ %v", answer)
 }
