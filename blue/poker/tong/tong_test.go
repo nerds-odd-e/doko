e391dd645b5f3418cde0086f6da8fbbfffa58e41
Thanakan Thanakwang <thanakan@odds.team>
2022-07-27 10:51:31 +0700

    tcring

--- blue/poker/tong/tong_test.go
+++ blue/poker/tong/tong_test.go
@@ -6,7 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTong(t *testing.T) {
+func TestTongEmpty(t *testing.T) {
 	result := IsPlayer1WinByTong([]string{})
-	assert.Equal(t, result, false, "Should be false")
+	assert.Equal(t, result, false, "Input is empty game, should be false")
+}
+
+func TestTongOneGameWin(t *testing.T) {
+	result := IsPlayer1WinByTong([]string{"KC KS KA 5H 4S 7D AS 5D AD 3C"})
+	assert.Equal(t, result, false, "Input is 3 of kind cards in p1 hand, should be true")
 }
