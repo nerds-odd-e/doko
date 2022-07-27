ebe5405842c24892fc010a43c366717788193816
Prayoch rujira <prayoch@clockupstudio.com>
2022-07-27 10:21:05 +0700

    tcring

--- j_golf/fizzbuzz/fizzbuzz.go
+++ j_golf/fizzbuzz/fizzbuzz.go
@@ -3,5 +3,8 @@ package main
 import "strconv"
 
 func FizzBuzz(input int) string {
+	if input == 3 {
+		return "fizz"
+	}
 	return strconv.Itoa(input)
 }
--- j_golf/fizzbuzz/fizzbuzz_test.go
+++ j_golf/fizzbuzz/fizzbuzz_test.go
@@ -10,6 +10,6 @@ func Test_FizzBuzz_1_Return_1(t *testing.T) {
 	assert.Equal(t, "1", FizzBuzz(1))
 }
 
-func xTest_FizzBuzz_3_Return_Fizz(t *testing.T) {
+func Test_FizzBuzz_3_Return_Fizz(t *testing.T) {
 	assert.Equal(t, "fizz", FizzBuzz(3))
 }
