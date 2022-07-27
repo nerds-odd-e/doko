b9a01342686f2fc2e44960666e7030c0ae1ef0ec
Prayoch rujira <prayoch@clockupstudio.com>
2022-07-27 12:04:20 +0700

    tcring

--- red/onepair_test.go
+++ red/onepair_test.go
@@ -9,6 +9,7 @@ import (
 func Test_IsOnePair(t *testing.T) {
 	assert.Equal(t, true, IsOnePair([]string{"AC", "AS", "2D", "3H", "5S"}))
 	assert.Equal(t, true, IsOnePair([]string{"KC", "AS", "AD", "3H", "5S"}))
+	assert.Equal(t, false, IsOnePair([]string{"KC", "KS", "AD", "3H", "5S"}))
 
 	assert.Equal(t, false, IsOnePair([]string{"AC", "KS", "2D", "3H", "5S"}))
 	assert.Equal(t, false, IsOnePair([]string{"TC", "AS", "2D", "3H", "5S"}))
