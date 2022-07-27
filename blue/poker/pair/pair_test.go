aaea9d5e23ca83865ca6a46dad1f1b405d8cefc1
Krittamet <krittamet.ko@baac.or.th>
2022-07-27 11:01:43 +0700

    tcring

--- blue/poker/pair/pair_test.go
+++ blue/poker/pair/pair_test.go
@@ -10,6 +10,7 @@ import (
 const p1WinPair = "AC TS KC AH 4S 7D 2S 5D 3S 3C"
 const p2WinPair = "8C TS KC 5H 4S 7D AS 5D AD 3C"
 const p1WinPair2 = "AC AS KC 5H 4S 7D 2S 5D AD 3C"
+const p1WinPairWithK = "9C 6S KC 5H KS 7D 2S 5D AD 3C"
 
 func TestPairP1Win(t *testing.T) {
 	assert.Equal(t, true, pair.IsPair(p1WinPair))
@@ -21,4 +22,8 @@ func TestPairP2Win(t *testing.T) {
 
 func TestPairP1Win2(t *testing.T) {
 	assert.Equal(t, true, pair.IsPair(p1WinPair2))
+}
+
+func xTestPairP1WinWithK(t *testing.T) {
+	assert.Equal(t, true, pair.IsPair(p1WinPairWithK))
 }
