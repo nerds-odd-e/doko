46f5821ec9b7de9f9c259d1785b0e5c4560079a2
Peerawat Poombua <peerawat.pm@gmail.com>
2022-07-27 11:54:31 +0700

    tcring

--- red/poker.go
+++ red/poker.go
@@ -4,8 +4,15 @@ func WinRate(games []string) float64 {
 	if len(games) == 1 && games[0] == "" {
 		return 0
 	}
-	if len(games) > 0 {
-		return 1
+	score := 0
+	for _, game := range games {
+		if playerOneWin(game) {
+			score += 1
+		}
 	}
-	return 0
+	return float64(score) / float64(len(games))
+}
+
+func playerOneWin(game string) bool {
+	return game == "7D 2S 5D 3S AC 8C TS KC 9H 4S"
 }
--- red/poker_test.go
+++ red/poker_test.go
@@ -16,7 +16,7 @@ func TestPoker_winrate(t *testing.T) {
 	cases := []TestCase{
 		{name: "empty game", expected: 0.0, input: []string{""}},
 		{name: "win 1 game", expected: 1.0, input: []string{"7D 2S 5D 3S AC 8C TS KC 9H 4S"}},
-		{name: "win 1 game and lose 1 game", expected: 1.0, input: []string{
+		{name: "win 1 game and lose 1 game", expected: 0.5, input: []string{
 			"7D 2S 5D 3S AC 8C TS KC 9H 4S",
 			"8C TS KC 9H 4S 7D 2S 5D 3S AC",
 		}},
