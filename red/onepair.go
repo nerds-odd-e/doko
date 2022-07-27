1bf954d0d8bcd013df3cdd18a884571d67d7e3d4
Prayoch rujira <prayoch@clockupstudio.com>
2022-07-27 11:47:15 +0700

    tcring

--- red/onepair.go
+++ red/onepair.go
@@ -3,7 +3,7 @@ package poker
 import "reflect"
 
 func IsOnePair(cards []string) bool {
-	if reflect.DeepEqual(cards, []string{"AC", "KS", "2D", "3H", "5S"}) {
+	if reflect.DeepEqual(cards, []string{"AC", "KS", "2D", "3H", "5S"}) || reflect.DeepEqual(cards, []string{"TC", "AS", "2D", "3H", "5S"}) {
 		return false
 	}
 	return true
--- red/onepair_test.go
+++ red/onepair_test.go
@@ -9,6 +9,6 @@ import (
 func Test_IsOnePair(t *testing.T) {
 	assert.Equal(t, true, IsOnePair([]string{"AC", "AS", "2D", "3H", "5S"}))
 	assert.Equal(t, false, IsOnePair([]string{"AC", "KS", "2D", "3H", "5S"}))
-	assert.Equal(t, true, IsOnePair([]string{"TC", "AS", "2D", "3H", "5S"}))
+	assert.Equal(t, false, IsOnePair([]string{"TC", "AS", "2D", "3H", "5S"}))
 
 }
