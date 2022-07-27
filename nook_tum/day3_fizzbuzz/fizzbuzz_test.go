95b963269f63bc889d51348a0ea2c7221feb408b
sakkatos <sakkatos1980@hotmail.com>
2022-07-27 10:29:36 +0700

    tcring

--- nook_tum/day3_fizzbuzz/fizzbuzz_test.go
+++ nook_tum/day3_fizzbuzz/fizzbuzz_test.go
@@ -11,3 +11,9 @@ func Test_Input1_say1(t *testing.T){
 	expected := "1"
 	assert.Equal(t, expected, answer, "ต้องการ 1 ได้ %v", answer)
 }
+
+func Test_Input3_sayFizz(t *testing.T){
+	answer := FizzBuzz(3)
+	expected := "1"
+	assert.Equal(t, expected, answer, "ต้องการ Fizz ได้ %v", answer)
+}
