5eecb26f018589fe00da202b599ded5e57c51bda
Krittamet <krittamet.ko@baac.or.th>
2022-07-27 13:28:11 +0700

    tcring

--- blue/poker/pair/pair_test.go
+++ blue/poker/pair/pair_test.go
@@ -11,24 +11,27 @@ const p1WinPair = "AC TS KC AH 4S 7D 2S 5D 3S 3C"
 const p2WinPair = "8C TS KC 5H 4S 7D AS 5D AD 3C"
 const p1WinPair2 = "AC AS KC 5H 4S 7D 2S 5D AD 3C"
 const p1WinPairWithK = "9C 6S KC 5H KS 7D 2S 5D AD 3C"
-const p2WinPairWithHightCard = "9C 6S KC 5H KS 7D 2S 5D AD AC"
 
-func TestPairP1Win(t *testing.T) {
-	assert.Equal(t, true, pair.IsPair(p1WinPair))
-}
+// const p2WinPairWithHightCard = "9C 6S KC 5H KS 7D 2S 5D AD AC"
 
-func TestPairP2Win(t *testing.T) {
-	assert.Equal(t, false, pair.IsPair(p2WinPair))
-}
+func TestPokerPair(t *testing.T) {
+	t.Run("Test Pair P1 Win", func(t *testing.T) {
+		assert.Equal(t, true, pair.IsPair(p1WinPair))
+	})
 
-func TestPairP1Win2(t *testing.T) {
-	assert.Equal(t, true, pair.IsPair(p1WinPair2))
-}
+	t.Run("Test Pair P2 Win", func(t *testing.T) {
+		assert.Equal(t, false, pair.IsPair(p2WinPair))
+	})
 
-func TestPairP1WinWithK(t *testing.T) {
-	assert.Equal(t, true, pair.IsPair(p1WinPairWithK))
-}
+	t.Run("Test Pair P1 Win2", func(t *testing.T) {
+		assert.Equal(t, true, pair.IsPair(p1WinPair2))
+	})
 
-func xTestPairP1Lose(t *testing.T) {
-	assert.Equal(t, false, pair.IsPair(p2WinPairWithHightCard))
+	t.Run("TestPairP1WinWithK", func(t *testing.T) {
+		assert.Equal(t, true, pair.IsPair(p1WinPairWithK))
+	})
+
+	// t.Run("xTestPairP1Lose", func(t *testing.T) {
+	// 	assert.Equal(t, false, pair.IsPair(p2WinPairWithHightCard))
+	// })
 }
