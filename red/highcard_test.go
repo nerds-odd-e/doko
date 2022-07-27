5432e0d987180a5854a507a85f9ff871793e5520
sakkatos <sakkatos1980@hotmail.com>
2022-07-27 11:57:50 +0700

    tcring

--- red/highcard_test.go
+++ red/highcard_test.go
@@ -10,3 +10,9 @@ func TestHighCard_Player1Win(t *testing.T) {
 	expected := true
 	assert.Equal(t, expected, answer, "ต้องการ true ได้ %v", answer)
 }
+
+func TestHighCard_Player1Lose(t *testing.T) {
+	answer :=  HighCardWin([]string{"4D", "1D", "2S", "2H", "2C"},[]string{"7D", "2D", "2S", "2H", "2C"})
+	expected := true
+	assert.Equal(t, expected, answer, "ต้องการ true ได้ %v", answer)
+}
