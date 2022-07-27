ea267969929ebf23809d1189fa1b3acd3c3ffd08
Krittamet <krittamet.ko@baac.or.th>
2022-07-27 09:57:08 +0700

    tcring

---
+++ jade_hok/fizzbuzz/fizzbuzz.go
@@ -0,0 +1,5 @@
+package fizzbuzz
+
+func FizzBuzz(number int) string {
+	return "1"
+}
--- jade_hok/fizzbuzz/fizzbuzz_test.go
+++ jade_hok/fizzbuzz/fizzbuzz_test.go
@@ -1,7 +1,16 @@
 package fizzbuzz_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"tdd.com/v1/pong-chet/fizzbuzz"
+)
 
 func Test_FizzBuzz_input_1_output_1(t *testing.T) {
+
+	result := fizzbuzz.FizzBuzz(1)
+
+	assert.Equal(t, "1", result)
 
 }
