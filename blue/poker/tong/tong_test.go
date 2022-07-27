8c0511f402a104cf8412d9f3cc7aaee8d9106200
Thanakan Thanakwang <thanakan@odds.team>
2022-07-27 10:57:23 +0700

    tcring

--- blue/poker/tong/tong_test.go
+++ blue/poker/tong/tong_test.go
@@ -15,3 +15,8 @@ func TestTongOneGameWin(t *testing.T) {
 	result := IsPlayer1WinByTong([]string{"KC KS KA 5H 4S 7D AS 5D AD 3C"})
 	assert.Equal(t, result, true, "Input is 3 of kind cards in p1 hand, should be true")
 }
+
+func TestTongOneGameLose(t *testing.T) {
+	result := IsPlayer1WinByTong([]string{"7D AS 5D AD 3C KC KS KA 5H 4S"})
+	assert.Equal(t, result, true, "Input is 3 of kind cards in p2 hand, should be false")
+}
