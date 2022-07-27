fe5a85a5b4ea3d028188d2dd66ed35e134e81e8b
Krittamet <krittamet.ko@baac.or.th>
2022-07-27 13:21:31 +0700

    tcring

--- blue/poker/pair/pair.go
+++ blue/poker/pair/pair.go
@@ -7,16 +7,6 @@ func IsPair(game string) bool {
 	p1 := cards[0:5]
 	// p2 := cards[5:]
 
-	// for i := 0; i < len(p2); i++ {
-	// 	for j := i + 1; j < len(p2); j++ {
-	// 		returnValue := isMatchCard(p2, i, j)
-	// 		if returnValue {
-	// 			return returnValue
-	// 		}
-
-	// 	}
-	// }
-
 	for i := 0; i < len(p1); i++ {
 		for j := i + 1; j < len(p1); j++ {
 			returnValue := isMatchCard(p1, i, j)
@@ -36,10 +26,3 @@ func isMatchCard(p1 []string, i int, j int) bool {
 	}
 	return false
 }
-
-// func getMatchCardScore(p1 []string, i int, j int) int {
-// 	if p1[i][0] == p1[j][0] {
-// 		return p1[i][0]
-// 	}
-// 	return 0
-// }
