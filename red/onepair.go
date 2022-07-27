a9d8ae8c4773536fefafa4148be4a62618c0aff9
Prayoch rujira <prayoch@clockupstudio.com>
2022-07-27 11:54:46 +0700

    tcring

--- red/onepair.go
+++ red/onepair.go
@@ -12,8 +12,5 @@ func IsOnePair(cards []string) bool {
 			return false
 		}
 	}
-	// if reflect.DeepEqual(cards, []string{"AC", "KS", "2D", "3H", "5S"}) || reflect.DeepEqual(cards, []string{"TC", "AS", "2D", "3H", "5S"}) {
-	// 	return false
-	// }
 	return true
 }
