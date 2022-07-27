9a600a9312f2c6218a72fa6381ff9eca37197d31
Thanakan Thanakwang <thanakan@odds.team>
2022-07-27 10:05:12 +0700

    tcring

--- MoAndKan/fizzBuzz/main_test.go
+++ MoAndKan/fizzBuzz/main_test.go
@@ -15,3 +15,8 @@ func TestFizzBuzzWithInputThree(t *testing.T) {
 	result := fizzBuzz(3)
 	assert.Equal(t, result, "fizz", "Input is 3, should get fizz")
 }
+
+func TestFizzBuzzWithInputFive(t *testing.T) {
+	result := fizzBuzz(5)
+	assert.Equal(t, result, "5", "Input is 5, should get buzz")
+}
