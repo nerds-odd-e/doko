6214bbef7b995395c3b7fba09e1f41f4b62cb79f
Suppasit Beer <82632771+suppasit-odds@users.noreply.github.com>
2022-07-27 10:11:12 +0700

    tcring

--- beer_o/fizzbuzz/fizzbuzz_test.go
+++ beer_o/fizzbuzz/fizzbuzz_test.go
@@ -7,19 +7,14 @@ import (
 )
 
 func TestFizzBuzz(t *testing.T) {
-	t.Run("it should say 1 if enter 1", func(t *testing.T) {
+	t.Run("it should return number by default", func(t *testing.T) {
 		assert.Equal(t, Fizzbuzz(1), "1")
-	})
-
-	t.Run("it should say 2 if enter 2", func(t *testing.T) {
 		assert.Equal(t, Fizzbuzz(2), "2")
 	})
 
-	t.Run("it should say fizz if enter 3", func(t *testing.T) {
+	t.Run("it should say fizz if input devisible by 3", func(t *testing.T) {
 		assert.Equal(t, Fizzbuzz(3), "fizz")
-	})
-
-	t.Run("it should say fizz if enter 6", func(t *testing.T) {
 		assert.Equal(t, Fizzbuzz(6), "fizz")
+		assert.Equal(t, Fizzbuzz(9), "fizz")
 	})
 }
