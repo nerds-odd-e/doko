8bfe785b9a224d573274cd171f95a828549cf53e
Thanakan Thanakwang <thanakan@odds.team>
2022-07-27 12:23:37 +0700

    tcring

--- blue/poker/poker_test.go
+++ blue/poker/poker_test.go
@@ -15,6 +15,10 @@ func TestWinrate(t *testing.T) {
 		game := []string{"AC 9S 7D 3C 2C QS 5C 4H 3D 2C"}
 		assert.Equal(t, 1.0, P1Winrate(game), "Input is player 1 win one game, should get 1.0")
 	})
+	t.Run("P1 lose one game", func(t *testing.T) {
+		game := []string{"QS 5C 4H 3D 2C AC 9S 7D 3C 2C"}
+		assert.Equal(t, 1.0, P1Winrate(game), "Input is player 1 lose one game, should get 0.0")
+	})
 }
 
 func TestHighCard(t *testing.T) {
