f58681af5ea149e051af3a9f70c9462474a28024
sakkatos <sakkatos1980@hotmail.com>
2022-07-27 09:56:25 +0700

    tcring

---
+++ nook_tum/day3_fizzbuzz/fizzbuzz_test.go
@@ -0,0 +1,13 @@
+package fizzbuzz
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Input1_say1(t *testing.T){
+	answer := FizzBuzz(1)
+	expected := ""
+	assert.Equal(t, expected, answer, "ต้องการ 1 ได้ %v", answer)
+}
