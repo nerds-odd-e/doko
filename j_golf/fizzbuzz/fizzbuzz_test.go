975fc116f2218b029ad72557e154c9e45cd673e0
Prayoch rujira <prayoch@clockupstudio.com>
2022-07-27 10:18:28 +0700

    tcring

--- j_golf/fizzbuzz/fizzbuzz_test.go
+++ j_golf/fizzbuzz/fizzbuzz_test.go
@@ -7,10 +7,5 @@ import (
 )
 
 func Test_FizzBuzz_1_Return_1(t *testing.T) {
-	input := 1
-	expect := "1"
-
-	actual := FizzBuzz(input)
-
-	assert.Equal(t, expect, actual)
+	assert.Equal(t, "1", FizzBuzz(1))
 }
