1f58cbf6a0a96bfa7ba8dec77f59764239eb13b3
Prayoch rujira <prayoch@clockupstudio.com>
2022-07-27 10:16:22 +0700

    tcring

--- j_golf/fizzbuzz/fizzbuzz_test.go
+++ j_golf/fizzbuzz/fizzbuzz_test.go
@@ -1,7 +1,16 @@
 package main
 
-import("testing")
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_FizzBuzz_1_Return_1(t *testing.T) {
+	input := 1
+	expect := "1"
 
+	actual := FizzBuzz(input)
+
+	assert.Equal(t, expect, actual)
 }
