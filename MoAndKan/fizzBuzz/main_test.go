8750f7cacf82341b44ef022837b96f3c07b003ed
Thanakan Thanakwang <thanakan@odds.team>
2022-07-27 10:16:11 +0700

    tcring

--- MoAndKan/fizzBuzz/main_test.go
+++ MoAndKan/fizzBuzz/main_test.go
@@ -6,11 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFizzBuzz(t *testing.T) {
-	result := fizzBuzz(1)
-	assert.Equal(t, result, "1", "Input is 1, should get 1")
-}
-
 func TestFizzBuzzWithInputThree(t *testing.T) {
 	result := fizzBuzz(3)
 	assert.Equal(t, result, "fizz", "Input is 3, should get fizz")
@@ -19,4 +14,22 @@ func TestFizzBuzzWithInputThree(t *testing.T) {
 func TestFizzBuzzWithInputFive(t *testing.T) {
 	result := fizzBuzz(5)
 	assert.Equal(t, result, "Buzz", "Input is 5, should get Buzz")
+}
+
+type TestCase struct {
+	name       string
+	input      int
+	expected   string
+	errMessage string
+}
+
+func TestFizz(t *testing.T) {
+	testCases := []TestCase{
+		TestCase{"Input is 1", 1, "1", "Input is 1, should get 1"},
+	}
+
+	for _, testCase := range testCases {
+		result := fizzBuzz(testCase.input)
+		assert.Equal(t, result, testCase.expected, testCase.errMessage)
+	}
 }
