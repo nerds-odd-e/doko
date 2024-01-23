95a6797920e7b9f04836e75f2fc5fa34fe622860
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-01-23 22:37:02 +0800

    move db init scripts to devbox.json

--- shell.nix
+++ shell.nix
@@ -5,9 +5,9 @@ let
   apple_sdk = darwin.apple_sdk.frameworks;
 in mkShell {
   name = "doko";
-  PGSQL_HOME = builtins.getEnv "PGSQL_HOME";
-  PGSQL_DATADIR = builtins.getEnv "PGSQL_DATADIR";
-  PGSQL_TCP_PORT = builtins.getEnv "PGSQL_TCP_PORT";
+  PG_HOME = builtins.getEnv "PG_HOME";
+  PGDATA = builtins.getEnv "PGDATA";
+  PG_TCP_PORT = builtins.getEnv "PG_TCP_PORT";
   buildInputs = [
     bash
     zsh
@@ -70,25 +70,25 @@ in mkShell {
     export GOROOT="$(readlink -e $(type -p go) | sed  -e 's/\/bin\/go//g')"
     export NODE_HOME=${pkgs.nodejs_20}
 
-    export PGSQL_BASEDIR=${pkgs.postgresql_16}
-    export PGSQL_HOME="''${PGSQL_HOME:-$PWD/pgsql}"
-    export PGSQL_DATADIR="''${PGSQL_DATADIR:-$PGSQL_HOME/data}"
-    export PGSQL_PID_FILE=$PGSQL_HOME/pgsql.pid
-    export PGSQL_TCP_PORT="''${PGSQL_TCP_PORT:-5432}"
-    export PGSQL_ROOTUSER=postgres
-    export PGSQL_DBUSER=doko
-    export PGSQL_DOKO_DEV_DB=doko_development
-    export PGSQL_DOKO_TEST_DB=doko_test
-    export PGSQL_DOKO_E2E_TEST_DB=doko_e2e_test
-    export PGSQL_TDD_DB=tdd
-    export PGSQL_TDD_TEST_DB=tdd_test
+    export PG_BASEDIR=${pkgs.postgresql_16}
+    export PG_HOME="''${PG_HOME:-$PWD/pgsql}"
+    export PGDATA="''${PGDATA:-$PG_HOME/data}"
+    export PG_PID_FILE=$PG_HOME/pgsql.pid
+    export PG_TCP_PORT="''${PG_TCP_PORT:-5432}"
+    export PG_ROOTUSER=postgres
+    export PG_DBUSER=doko
+    export PG_DOKO_DEV_DB=doko_development
+    export PG_DOKO_TEST_DB=doko_test
+    export PG_DOKO_E2E_TEST_DB=doko_e2e_test
+    export PG_TDD_DB=tdd
+    export PG_TDD_TEST_DB=tdd_test
 
-    export PATH=$GOROOT/bin:$GOPATH/bin:$NODE_HOME/bin:$PGSQL_BASEDIR/bin:$PATH
+    export PATH=$GOROOT/bin:$NODE_HOME/bin:$PG_BASEDIR/bin:$PATH
 
     export CGO_ENABLED=1
     export MallocNanoZone=0 # to disable OSX automatic memory allocation before ruuning tests
-    export DB_NAME=$PGSQL_DOKO_TDD_DB
-    export DB_USER=$PGSQL_ROOTUSER
+    export DB_NAME=$PG_TDD_DB
+    export DB_USER=$PG_ROOTUSER
     export DB_PASSWORD=password
 
     generate_dotenv() {
@@ -102,12 +102,12 @@ in mkShell {
     cleanup()
     {
       echo -e "\nBYE!!! EXITING doko NIX DEVELOPMENT ENVIRONMENT."
-      export PGSQLD_PID=$(ps -ax | grep -v " grep " | grep $PGSQL_BASEDIR/bin/postgres | awk '{ print $1 }')
+      export PG_PID=$(ps -ax | grep -v " grep " | grep $PG_BASEDIR/bin/postgres | awk '{ print $1 }')
 
-      if [[ ! -z "$PGSQLD_PID" ]]; then
-        echo -e "\n\tPostgreSQL Server still running on Port: $PGSQL_TCP_PORT, at PID: $PGSQLD_PID"
+      if [[ ! -z "$PG_PID" ]]; then
+        echo -e "\n\tPostgreSQL Server still running on Port: $PG_TCP_PORT, at PID: $PG_PID"
         echo -e "\tYou may choose to SHUTDOWN PostgreSQL Server by issuing:"
-        echo -e "\t'$PGSQL_BASEDIR/bin/pg_ctl -D $PGSQL_DATADIR stop && rm -f $PGSQL_HOME/logfile'\n"
+        echo -e "\t'$PG_BASEDIR/bin/pg_ctl -D $PGDATA stop && rm -f $PG_HOME/logfile'\n"
       fi
     }
 
@@ -117,10 +117,10 @@ in mkShell {
     echo "##   DOKO_ROOT: $DOKO_ROOT                          "
     echo "##   GOROOT: $GOROOT                                "
     echo "##   NODE_HOME: $NODE_HOME                          "
-    echo "##   PGSQL_BASEDIR: $PGSQL_BASEDIR                  "
-    echo "##   PGSQL_HOME: $PGSQL_HOME                        "
-    echo "##   PGSQL_DATADIR: $PGSQL_DATADIR                  "
-    echo "##   PGSQL_TCP_PORT: $PGSQL_TCP_PORT                "
+    echo "##   PG_BASEDIR: $PG_BASEDIR                        "
+    echo "##   PG_HOME: $PG_HOME                           "
+    echo "##   PGDATA: $PGDATA                                "
+    echo "##   PG_TCP_PORT: $PG_TCP_PORT                      "
     echo "                                                                                "
     echo "###################################################################################################################"
 
@@ -128,11 +128,11 @@ in mkShell {
 
     start_psql_db_cluster
 
-    echo -e "Create DB user/s..." && create_doko_db_user $PGSQL_ROOTUSER
+    echo -e "Create DB user/s..." && create_doko_db_user $PG_ROOTUSER
 
     echo -e "Create databases..."
-    create_doko_db $PGSQL_TDD_DB
-    create_doko_db $PGSQL_TDD_TEST_DB
+    create_doko_db $PG_TDD_DB
+    create_doko_db $PG_TDD_TEST_DB
 
     export GPG_TTY=$(tty)
     export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
